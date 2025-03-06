44fcbbb9d94af3ea3c951eaedb92c1096781cf2e
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-03-06 18:44:16 +0530

    Create file

---
+++ 2965-FindMissingandRepeatedValues/2965-FindMissingandRepeatedValues.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        vector<int> ans;
+        vector<int> map;
+        int n = grid.size();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int current = grid[i][j];
+                if (find(map.begin(), map.end(), current) != map.end()) {
+                    ans.push_back(current);
+                }
+                map.push_back(current);
+            }
+        }
+
+        for (int i = 1; i <= (n * n); i++) {
+            if (find(map.begin(), map.end(), i) == map.end()) {
+                ans.push_back(i);
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
