4bef8bd577654110021e3b3025cd08ae8b9cd132
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-03-09 08:17:38 +0530

    Create file

---
+++ Q1-FruitsIntoBasketsII/Q1-FruitsIntoBasketsII.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canBePlaced(int fruit, vector<int>& baskets){
+        for(int i=0;i<baskets.size();i++){
+            if(baskets[i]!=0 && baskets[i]>=fruit){
+                baskets[i]=0;
+                return true;
+            }
+        }
+        return false;
+    }
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int count=0;
+        for(int i=0;i<fruits.size();i++){
+            int fruit = fruits[i];
+            bool res= canBePlaced(fruit, baskets);
+            if(res==false)
+                count++;
+        }
+        return count;
+    }
+};
