f59437dcbdb30b28c3c6c8ee32f2a0007dfb6f2c
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-04-18 00:14:45 +0530

    File updated at 4/18/2025, 12:14:44 AM

--- 78-Subsets/78-Subsets.cpp
+++ 78-Subsets/78-Subsets.cpp
@@ -1,21 +1,23 @@
+// Last updated: 4/18/2025, 12:14:44 AM
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> subsets;
-        createSubsets(nums, 0, res, subsets);
-        return res;
-    }
-    void createSubsets(vector<int> & nums, int index, vector<vector<int>> &res, vector<int> &subsets){
-        if(index==nums.size()){
+    void getSubsets(vector<int>& nums, int index,int n, vector<vector<int>> &res, vector<int>& subsets){
+        if(index==n)
+        {
             res.push_back(subsets);
             return;
         }
-
         subsets.push_back(nums[index]);
-        createSubsets(nums, index+1, res, subsets);
-
+        getSubsets(nums, index+1, n, res, subsets);
         subsets.pop_back();
-        createSubsets(nums, index+1, res, subsets);
+        getSubsets(nums, index+1, n, res, subsets);
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        int n=nums.size();
+        vector<int> subsets;
+        getSubsets(nums, 0,n, res, subsets);
+        return res;
     }
 };
