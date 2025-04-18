feda9e52ae443a01878fa6dde6c329c788546e46
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-04-18 23:34:50 +0530

    File updated at 4/18/2025, 11:34:49 PM

--- 70-ClimbingStairs/70-ClimbingStairs.cpp
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -1,17 +1,14 @@
-// Last updated: 4/18/2025, 11:32:01 PM
+// Last updated: 4/18/2025, 11:34:49 PM
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 0 || n == 1) return 1;
-
-        int a = 1, b = 1, temp;
-
-        for (int i = 2; i <= n; ++i) {
-            temp = a + b;
-            a = b;
-            b = temp;
+        if(n==0 || n==1) return 1;
+        int prev1=1, prev2=1, curr=0;
+        for(int i=2;i<=n;i++){
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
         }
-
-        return b;
+        return prev1;
     }
 };
