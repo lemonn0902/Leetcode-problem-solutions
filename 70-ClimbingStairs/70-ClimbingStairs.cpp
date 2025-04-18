0c0858a9acf364a85c34700cba203d47f11435fe
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-04-18 23:32:01 +0530

    File updated at 4/18/2025, 11:32:01 PM

--- 70-ClimbingStairs/70-ClimbingStairs.cpp
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -1,17 +1,17 @@
-// Last updated: 4/18/2025, 11:31:22 PM
+// Last updated: 4/18/2025, 11:32:01 PM
 class Solution {
 public:
     int climbStairs(int n) {
         if (n == 0 || n == 1) return 1;
-        
-        vector<int> dp(n + 1);
-        dp[0] = 1;
-        dp[1] = 1;
+
+        int a = 1, b = 1, temp;
 
         for (int i = 2; i <= n; ++i) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            temp = a + b;
+            a = b;
+            b = temp;
         }
 
-        return dp[n];
+        return b;
     }
 };
