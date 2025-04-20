4a0bc04d4fdeda04cf2357079c6b7e21f9051879
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-04-21 01:10:00 +0530

    Create file

---
+++ 213-HouseRobberII/213-HouseRobberII.cpp
@@ -0,0 +1,23 @@
+// Last updated: 4/21/2025, 1:09:59 AM
+class Solution {
+public:
+    int robLinear(vector<int>& nums, int index, int start, int end, vector<int>&dp){
+        if(index==start) return nums[start];
+        if(index==start+1) return max(nums[start], nums[start+1]);
+
+        if(dp[index-start]!=-1) return dp[index-start];
+        int rob=robLinear(nums, index-2,start,end,dp)+nums[index];
+        int skip=robLinear(nums, index-1, start,end,dp);
+        return dp[index-start]=max(rob,skip);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        vector<int> dp1(n-1,-1);
+        vector<int> dp2(n-1, -1);
+
+        int case1= robLinear(nums, n-2, 0, n-2,dp1);
+        int case2= robLinear(nums, n-1, 1, n-1,dp2);
+        return max(case1, case2);
+    }
+};
