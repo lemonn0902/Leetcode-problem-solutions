37d2ca7ce61ed4462f178ac630fea543fe4ca9eb
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-04-23 02:04:56 +0530

    File updated at 4/23/2025, 2:04:56 AM

--- 62-UniquePaths/62-UniquePaths.cpp
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/23/2025, 1:49:43 AM
+// Last updated: 4/23/2025, 2:04:56 AM
 class Solution {
 public:
     int f(int i , int j, vector<vector<int>>& dp){
@@ -14,6 +14,22 @@ public:
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n,-1));
-        return f(m-1, n-1,dp);
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)
+                dp[i][j]=1;
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+
+                }
+
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
