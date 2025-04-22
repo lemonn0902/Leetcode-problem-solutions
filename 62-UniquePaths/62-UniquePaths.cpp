e10c060b006de83ff029c15f0774b8cccc09b466
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-04-23 02:05:35 +0530

    File updated at 4/23/2025, 2:05:35 AM

--- 62-UniquePaths/62-UniquePaths.cpp
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -1,17 +1,17 @@
-// Last updated: 4/23/2025, 2:04:56 AM
+// Last updated: 4/23/2025, 2:05:35 AM
 class Solution {
 public:
-    int f(int i , int j, vector<vector<int>>& dp){
-        //base case:
-        if(i==0 && j==0)//reached top
-        return 1;
-        if(i<0 || j<0) //out of bounds of the matrix cell
-        return 0; 
-        if(dp[i][j]!=-1) return dp[i][j];
-        int left= f(i, j-1,dp);
-        int right= f(i-1, j,dp);
-        return dp[i][j]=left+right;
-    }
+    // int f(int i , int j, vector<vector<int>>& dp){
+    //     //base case:
+    //     if(i==0 && j==0)//reached top
+    //     return 1;
+    //     if(i<0 || j<0) //out of bounds of the matrix cell
+    //     return 0; 
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int left= f(i, j-1,dp);
+    //     int right= f(i-1, j,dp);
+    //     return dp[i][j]=left+right;
+    // }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n,-1));
         dp[0][0]=1;
