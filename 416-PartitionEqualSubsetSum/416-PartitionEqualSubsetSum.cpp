977fb0a249566ab26a995a1def1eb3dcec712fb8
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-05-12 00:48:23 +0530

    File updated at 5/12/2025, 12:48:23 AM

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.cpp
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.cpp
@@ -1,35 +1,26 @@
-// Last updated: 5/1/2025, 12:35:30 AM
+// Last updated: 5/12/2025, 12:48:23 AM
 class Solution {
 public:
-    
+    bool f(int index, vector<int>& nums, int target , vector<vector<int>>& dp){
+        if(target==0) return true;
+        if(index==0) return nums[0]==target;
+        if(dp[index][target]!=-1) return dp[index][target];
+        bool notTake = f(index-1, nums, target, dp);
+        bool Take= false;
+        if(target>= nums[index]){
+            Take= f(index-1, nums, target-nums[index], dp);
+        }
+        return dp[index][target]= Take | notTake;
+    }
     bool canPartition(vector<int>& nums) {
-        int target=0, total=0;
-        int n= nums.size();
+        int target =0, total=0;
+        int n=nums.size();
         
-        
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<n;i++)
         total+=nums[i];
         target=total/2;
-        vector<vector<bool>> dp(n, vector<bool>(target+1, 0));
+        vector<vector<int>> dp(n, vector<int>(target+1, -1));
         if(total%2!=0) return false;
-        
-        else {
-            //base case:
-            for(int i=0;i<n;i++){
-                dp[i][0]= true;
-            }
-            if (nums[0] <= target) dp[0][nums[0]] = true;
-
-            for( int i=1;i<n;i++){
-                for(int t=1;t<=target;t++){
-                    bool notTake= dp[i-1][t];
-                    bool take= false;
-                    if(t>=nums[i]) take=dp[i-1][t-nums[i]];
-                    dp[i][t]= take | notTake;
-                }
-            }
-        }
-        return dp[n-1][target];
-        
+        else return f(n-1, nums, target, dp);
     }
 };
