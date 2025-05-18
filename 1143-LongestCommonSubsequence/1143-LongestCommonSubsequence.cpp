0c1f3aea7dcfe152a83081ed583dd798e9832860
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-05-18 12:37:50 +0530

    Create file

---
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
@@ -0,0 +1,17 @@
+// Last updated: 5/18/2025, 12:37:49 PM
+class Solution {
+public:
+    int f(int i, int j, string &text1, string &text2, vector<vector<int>>& dp){
+        if(i == 0 || j == 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(text1[i-1] == text2[j-1])
+            return dp[i][j] = 1 + f(i-1, j-1, text1, text2, dp);
+        return dp[i][j] = max(f(i-1, j, text1, text2, dp), f(i, j-1, text1, text2, dp));
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size(), m = text2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return f(n, m, text1, text2, dp);
+    }
+};
