326a8a58ee3fa50461cc8bb8271cfec4e8f9e973
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-05-27 20:15:27 +0530

    File updated at 5/27/2025, 8:15:27 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.cpp
+++ 115-DistinctSubsequences/115-DistinctSubsequences.cpp
@@ -1,4 +1,4 @@
-// Last updated: 5/27/2025, 8:15:11 PM
+// Last updated: 5/27/2025, 8:15:27 PM
 class Solution {
 public:
 //memoization: 
@@ -19,9 +19,6 @@ public:
         for(int i=0;i<=m;i++){
             dp[i][0]=1;
         }
-        // for(int j=0;j<=n;j++){
-        //     dp[0][j]=0;
-        // }
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(s[i-1]==t[j-1])
