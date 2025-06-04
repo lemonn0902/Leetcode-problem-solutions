db6affca4f8a865521e95fb11f6f35d254c8f819
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-06-05 01:12:17 +0530

    Create 123-BestTimetoBuyandSellStockIII

---
+++ 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.cpp
@@ -0,0 +1,26 @@
+// Last updated: 6/5/2025, 1:12:17 AM
+class Solution {
+public:
+    int f( int index, bool canBuy, vector<int>& prices, int transLeft, vector<vector<vector<int>>>& dp){
+        int n= prices.size();
+        if(index==n || transLeft==0){
+            return 0;
+        }
+        if(dp[index][canBuy][transLeft]!=-1) return dp[index][canBuy][transLeft];
+        if(canBuy){
+            int buy=-prices[index]+f(index+1, false, prices, transLeft,dp);
+            int skip= f(index+1, true, prices, transLeft,dp);
+            return dp[index][canBuy][transLeft]=max(buy, skip);
+        }else{
+            int sell= prices[index]+f(index+1, true, prices, transLeft-1,dp);
+            int skip= f(index+1, false, prices, transLeft,dp);
+            return dp[index][canBuy][transLeft]=max(sell, skip);
+        }
+        
+    }
+    int maxProfit(vector<int>& prices) {
+        int n= prices.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        return f(0, true, prices,2, dp);
+    }
+};
