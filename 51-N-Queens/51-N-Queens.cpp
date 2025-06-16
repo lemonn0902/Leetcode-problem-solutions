849018064c01644135f8da464f19dd9fd96762fb
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-06-16 13:25:29 +0530

    Create 51-N-Queens

---
+++ 51-N-Queens/51-N-Queens.cpp
@@ -0,0 +1,42 @@
+// Last updated: 6/16/2025, 1:25:29 PM
+class Solution {
+public:
+    bool isSafe(int row, int col, vector<string>& board, int n){
+        //check same column 
+        for(int i=0;i<row;i++){
+            if(board[i][col]=='Q') return false;
+        }
+        //check upper-left diagnol
+        for(int i=row-1,j=col-1;i >= 0 &&  j >= 0; i--, j--)
+        {
+            if (board[i][j] == 'Q') return false;
+        }
+        //check upper-right diagonal
+        for(int i=row-1, j=col+1; i >= 0 &&  j<n; i--, j++){
+            if (board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+    void backtrack(int n, int row, vector<vector<string>>& fin, vector<string>& board){
+        if(row==n){
+            fin.push_back(board);
+            return;
+        }
+        for(int col=0;col<n;col++){
+            if(isSafe(row, col, board, n)){
+                board[row][col]='Q';
+            backtrack(n, row+1, fin, board);
+            board[row][col]='.';
+            }
+            
+        }
+
+
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> fin;
+        vector<string> board(n, string(n, '.'));
+        backtrack(n,0, fin, board);
+        return fin;
+    }
+};
