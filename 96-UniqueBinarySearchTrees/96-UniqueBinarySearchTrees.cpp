499d222958e46259b0f665464b7f133ce22c01f1
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-06-26 01:07:13 +0530

    File updated at 6/26/2025, 1:07:13 AM

--- 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.cpp
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.cpp
@@ -1,23 +1,29 @@
-// Last updated: 6/26/2025, 12:44:17 AM
+// Last updated: 6/26/2025, 1:07:13 AM
 class Solution {
 public:
-    int f(int n, vector<int>& dp){
-        if(n<=1) return 1;
-        if(dp[n]!=-1) return dp[n];
-        int total =0;
+//memoizaton:
+    // int f(int n, vector<int>& dp){
+    //     if(n<=1) return 1;
+    //     if(dp[n]!=-1) return dp[n];
+    //     int total =0;
         
-        for(int root=1;root<=n;root++){
-            int left=f(root-1,dp);
-            int right=f(n-root,dp);
-            total=total+(left*right);
-        }
-        dp[n]=total;
-        return total;
+    //     for(int root=1;root<=n;root++){
+    //         int left=f(root-1,dp);
+    //         int right=f(n-root,dp);
+    //         total=total+(left*right);
+    //     }
+    //     dp[n]=total;
+    //     return total;
 
-    }
+    // }
     int numTrees(int n) {
-        vector<int> dp(n+1,-1);
-        return f(n,dp);
-
+        vector<int> dp(n+1,0);
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                dp[i]+=dp[j-1]*dp[i-j];
+            }
+        }
+        return dp[n];
     }
 };
