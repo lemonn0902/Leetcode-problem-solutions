fd05bcb7a60d30bd573f9a43dbbcb76e52c45787
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-07-02 00:55:16 +0530

    File updated at 7/2/2025, 12:55:16 AM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.cpp
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.cpp
@@ -1,9 +1,14 @@
-// Last updated: 7/2/2025, 12:54:32 AM
+// Last updated: 7/2/2025, 12:55:16 AM
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq(nums.begin(),nums.end());
-        while(pq.size()>k){
+        priority_queue<int>pq;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            pq.push(nums[i]);
+        }
+        k--;
+        while(k--){
             pq.pop();
         }
         return pq.top();
