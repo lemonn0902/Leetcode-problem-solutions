dfeb25d1eda0d6581007677d50eee8815007cad8
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-07-02 00:38:32 +0530

    Create 973-KClosestPointstoOrigin

---
+++ 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/2/2025, 12:38:32 AM
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<vector<int>> res;
+        priority_queue<pair<int, vector<int>>> pq;
+        for(auto& it: points){
+            int x=it[0];
+            int y=it[1];
+            int d=x * x + y * y;
+            pq.push({d, it});
+            if(pq.size()>k) pq.pop();
+        }
+        
+        while (!pq.empty()) {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return res;
+    }
+};
