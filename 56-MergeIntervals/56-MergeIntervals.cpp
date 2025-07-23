880c7e04da83e0caa513b0061853c136aa758d02
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-07-23 22:05:14 +0530

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/56-MergeIntervals.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/23/2025, 10:05:14 PM
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n= intervals.size();
+        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end());
+        for(int i=0;i<n;i++){
+            int start= intervals[i][0];
+            int end= intervals[i][1];
+            if(!res.empty() && end<=res.back()[1]){
+                continue;
+            }
+            for(int j=i+1;j<n;j++){
+                if(end>=intervals[j][0]){
+                    end= max(end, intervals[j][1]);
+                }else{
+                    break;
+                }
+            }
+            res.push_back({start,end});
+        }
+        return res;
+    }
+};
