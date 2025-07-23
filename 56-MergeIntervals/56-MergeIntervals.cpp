8b8e41804a63278d464c707c69b106f8242284cf
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-07-23 22:14:18 +0530

    File updated at 7/23/2025, 10:14:18 PM

--- 56-MergeIntervals/56-MergeIntervals.cpp
+++ 56-MergeIntervals/56-MergeIntervals.cpp
@@ -1,24 +1,16 @@
-// Last updated: 7/23/2025, 10:05:14 PM
+// Last updated: 7/23/2025, 10:14:18 PM
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n= intervals.size();
-        vector<vector<int>> res;
         sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
         for(int i=0;i<n;i++){
-            int start= intervals[i][0];
-            int end= intervals[i][1];
-            if(!res.empty() && end<=res.back()[1]){
-                continue;
+            if(res.empty()||intervals[i][0]>res.back()[1]){
+                res.push_back(intervals[i]);
+            }else{
+                res.back()[1]= max(res.back()[1], intervals[i][1]);
             }
-            for(int j=i+1;j<n;j++){
-                if(end>=intervals[j][0]){
-                    end= max(end, intervals[j][1]);
-                }else{
-                    break;
-                }
-            }
-            res.push_back({start,end});
         }
         return res;
     }
