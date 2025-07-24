92c0ae3da13c6ca0fa9947920a8a0aa2c9e8045f
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-07-24 10:54:36 +0530

    File updated at 7/24/2025, 10:54:35 AM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
@@ -1,18 +1,18 @@
-// Last updated: 7/24/2025, 12:34:50 AM
+// Last updated: 7/24/2025, 10:54:35 AM
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int,int> mpp;
         mpp[0]=1;
-        int preSum=0;
+        int total=0;
         int count=0;
         for(int i=0;i<nums.size();i++){
-            preSum+=nums[i];
-            if(mpp.find(preSum-k)!=mpp.end()){
-                count+= mpp[preSum-k];
+            total+=nums[i];
+            if(mpp.find(total-k)!=mpp.end()){
+                count+=mpp[total-k];
             }
-            mpp[preSum]++;
-            
+
+            mpp[total]++;
         }
         return count;
     }
