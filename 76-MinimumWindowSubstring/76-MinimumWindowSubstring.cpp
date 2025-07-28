6fa6cb71c07f23f38de6da8f1ae663aad6bdf89d
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-07-28 14:20:03 +0530

    Create 76-MinimumWindowSubstring

---
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.cpp
@@ -0,0 +1,38 @@
+// Last updated: 7/28/2025, 2:20:02 PM
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char,int> tFreq;
+        unordered_map<char,int> freq;
+        int m=s.size();
+        int n=t.size();
+        if(n>m) return "";
+        int l=0;
+        int r=0;
+        int minLen=INT_MAX;int startIndex=0; int formed=0; 
+        for(int i=0;i<n;i++){
+            tFreq[t[i]]++;
+        }
+        int req=tFreq.size();
+        while(r<m){
+            freq[s[r]]++;
+            if(tFreq.count(s[r])&&tFreq[s[r]]==freq[s[r]]){
+                formed++;
+            }
+            //now we try to shrink from left 
+            while(formed==req){
+                if(1+r-l<minLen){
+                    minLen=1+r-l;
+                    startIndex=l;
+                }
+                freq[s[l]]--;
+                if (tFreq.count(s[l]) && freq[s[l]] < tFreq[s[l]]) {
+                    formed--;
+                }
+                l++;
+            }
+            r++;
+        }
+        return minLen==INT_MAX? "": s.substr(startIndex, minLen);
+    }
+};
