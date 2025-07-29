8edc64c02bde9ef5c84d4f3792066d08d52c37fe
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-07-29 13:57:19 +0530

    File updated at 7/29/2025, 1:57:18 PM

--- 41-FirstMissingPositive/41-FirstMissingPositive.cpp
+++ 41-FirstMissingPositive/41-FirstMissingPositive.cpp
@@ -1,12 +1,13 @@
-// Last updated: 7/29/2025, 1:22:11 PM
+// Last updated: 7/29/2025, 1:57:18 PM
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n=nums.size();
-        if(find(nums.begin(), nums.end(), 1)==nums.end()) return 1;
+        //if(find(nums.begin(), nums.end(), 1)==nums.end()) return 1;
         
         for(int i=0;i<nums.size();i++){
             while(nums[i]>0&&nums[i]<=n&&nums[i]!= nums[nums[i]-1]){
+                
                 int temp=nums[i];
                 nums[i]=nums[temp-1];
                 nums[temp-1]=temp;
