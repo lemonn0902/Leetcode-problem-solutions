5b67538b9583f0c7eae58df2ad740a2665172600
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-07-29 13:22:11 +0530

    Create 41-FirstMissingPositive

---
+++ 41-FirstMissingPositive/41-FirstMissingPositive.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/29/2025, 1:22:11 PM
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        if(find(nums.begin(), nums.end(), 1)==nums.end()) return 1;
+        
+        for(int i=0;i<nums.size();i++){
+            while(nums[i]>0&&nums[i]<=n&&nums[i]!= nums[nums[i]-1]){
+                int temp=nums[i];
+                nums[i]=nums[temp-1];
+                nums[temp-1]=temp;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1) return i+1;
+        }
+        return n+1;
+    }
+};
