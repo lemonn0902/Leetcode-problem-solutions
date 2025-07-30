e59383456e78b2d8bd417b60381ac3f9191b7418
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-07-30 13:33:05 +0530

    File updated at 7/30/2025, 1:33:05 PM

--- 1047-RemoveAllAdjacentDuplicatesInString/1047-RemoveAllAdjacentDuplicatesInString.cpp
+++ 1047-RemoveAllAdjacentDuplicatesInString/1047-RemoveAllAdjacentDuplicatesInString.cpp
@@ -1,24 +1,16 @@
-// Last updated: 7/30/2025, 1:30:12 PM
+// Last updated: 7/30/2025, 1:33:05 PM
 class Solution {
 public:
     string removeDuplicates(string s) {
         if(s=="") return "";
         if(s.size()==1) return s;
-        stack<char> st;
+        string res="";
         int n= s.size();
         for(int i=0;i<n;i++){
-            if(!st.empty()&& st.top()==s[i]){
-                st.pop();
-                
-                
-            }
-            else st.push(s[i]);
-        }
-        string res="";
-        while(!st.empty()){
-            res=st.top()+res;
-            st.pop();
+            if(!res.empty()&& res.back()==s[i]) res.pop_back();
+            else res.push_back(s[i]);
         }
         return res;
+            
     }
 };
