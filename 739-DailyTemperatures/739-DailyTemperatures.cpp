6135a6f2f8e78c2c3a646940665b0475b64ce284
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-07-30 12:48:34 +0530

    Create 739-DailyTemperatures

---
+++ 739-DailyTemperatures/739-DailyTemperatures.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/30/2025, 12:48:34 PM
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> res;
+        stack<pair<int,int>> st;
+        int n= temperatures.size();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()&& st.top().first<=temperatures[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                res.push_back(0);
+            }
+            else{
+                res.push_back(st.top().second-i);
+            }
+            st.push({temperatures[i], i});
+        }
+        
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
