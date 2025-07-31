9c7116d251477e323b3419120df451aaf30b1f97
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-07-31 18:14:13 +0530

    File updated at 7/31/2025, 6:14:13 PM

--- 88-MergeSortedArray/88-MergeSortedArray.cpp
+++ 88-MergeSortedArray/88-MergeSortedArray.cpp
@@ -1,21 +1,22 @@
-// Last updated: 7/31/2025, 6:13:29 PM
+// Last updated: 7/31/2025, 6:14:13 PM
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i=m-1;
         int j=n-1;
-        int k=nums1.size()-1;
+        int k=m+n-1;
         while(i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
                 nums1[k]=nums1[i];
                 i--;
-                k--;
+                
             } 
-            else if(nums1[i]<=nums2[j]){
+            else {
                 nums1[k]=nums2[j];
                 j--;
-                k--;
+               
             }
+            k--;
         }
         while(j>=0){
             nums1[k]=nums2[j];
