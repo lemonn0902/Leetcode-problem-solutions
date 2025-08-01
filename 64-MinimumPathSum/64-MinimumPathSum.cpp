d7d39751cd87adffc252e272ded2b85a3245d587
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-08-01 12:27:37 +0530

    File updated at 8/1/2025, 12:27:37 PM

--- 64-MinimumPathSum/64-MinimumPathSum.cpp
+++ 64-MinimumPathSum/64-MinimumPathSum.cpp
@@ -1,19 +1,24 @@
-// Last updated: 4/24/2025, 5:38:43 PM
+// Last updated: 8/1/2025, 12:27:37 PM
 class Solution {
 public:
-    int f(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& dp){
-        //base case: 
-        if(i<0 || j<0) return 1e9;
-        if(i==0 && j==0) return grid[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int left= f(grid, i, j-1, dp) + grid[i][j];
-        int up= f(grid, i-1, j, dp) + grid[i][j];
-        return dp[i][j]=min(left, up);
+    int f(vector<vector<int>>& grid, int x,int y, vector<vector<int>>& dp){
+        int m=grid.size();
+        int n=grid[0].size();
+        if(x<0||x>=m) return 1e9;
+        if(y<0||y>=n) return 1e9;
+        if(x==m-1 && y==n-1) return grid[x][y];
+        if(dp[x][y]!=-1) return dp[x][y];
+        //move down
+        int d=f(grid, x+1,y,dp)+grid[x][y];
+        //move right
+        int r=f(grid, x, y+1, dp)+grid[x][y];
+        dp[x][y]=min(d,r);
+        return dp[x][y];
     }
     int minPathSum(vector<vector<int>>& grid) {
-        int m= grid.size();
-        int n= grid[0].size();
-        vector<vector<int>> dp(m, vector<int> (n,-1));
-        return f(grid, m-1, n-1, dp);
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        return f(grid,0,0,dp);
     }
 };
