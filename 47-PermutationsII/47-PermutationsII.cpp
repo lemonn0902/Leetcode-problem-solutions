1f23ff0db28e432accb936cde64446cc00c2795a
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-08-03 01:33:50 +0530

    Create 47-PermutationsII

---
+++ 47-PermutationsII/47-PermutationsII.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/3/2025, 1:33:50 AM
+class Solution {
+public:
+    void backtrack(vector<int>& nums,vector<int> perm,vector<bool> used,set<vector<int>>& res){
+        int n=nums.size();
+        //base case
+        if(perm.size()==n){
+            res.insert(perm);
+            return;
+        }
+        for(int i=0;i<n;i++){
+            if(used[i]==true) continue;
+            perm.push_back(nums[i]);
+            used[i]=true;
+            backtrack(nums, perm, used,res);
+            used[i]=false;
+            perm.pop_back();
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<int> perm;
+        set<vector<int>> res;
+        int n=nums.size();
+        vector<bool> used(n, false);
+        backtrack(nums, perm, used, res);
+        return vector<vector<int>>(res.begin(), res.end());
+    }
+};
