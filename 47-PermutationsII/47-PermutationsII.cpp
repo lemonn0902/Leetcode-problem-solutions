222a46396d0ae59296a74b0162cfded5ebeab542
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-08-03 01:43:16 +0530

    File updated at 8/3/2025, 1:43:16 AM

--- 47-PermutationsII/47-PermutationsII.cpp
+++ 47-PermutationsII/47-PermutationsII.cpp
@@ -1,15 +1,17 @@
-// Last updated: 8/3/2025, 1:37:20 AM
+// Last updated: 8/3/2025, 1:43:16 AM
 class Solution {
 public:
     void backtrack(vector<int>& nums,vector<int> perm,vector<bool> used,vector<vector<int>>& res){
         int n=nums.size();
         //base case
-        if (perm.size() == n && (find(res.begin(), res.end(), perm) == res.end())) {
+        if (perm.size() == n) {
             res.push_back(perm);
             return;
         }
         for(int i=0;i<n;i++){
             if(used[i]==true) continue;
+            //check for duplicate
+            if(i>0 && nums[i]==nums[i-1]&&!used[i-1]) continue;
             perm.push_back(nums[i]);
             used[i]=true;
             backtrack(nums, perm, used,res);
@@ -22,6 +24,7 @@ public:
         vector<vector<int>> res;
         int n=nums.size();
         vector<bool> used(n, false);
+        sort(nums.begin(), nums.end());
         backtrack(nums, perm, used, res);
         return res;
     }
