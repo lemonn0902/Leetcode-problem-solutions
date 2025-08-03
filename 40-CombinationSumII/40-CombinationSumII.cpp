8f69470d9e55f3f7126c7163816a27be9f6dc3e5
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-08-03 20:54:30 +0530

    File updated at 8/3/2025, 8:54:30 PM

--- 40-CombinationSumII/40-CombinationSumII.cpp
+++ 40-CombinationSumII/40-CombinationSumII.cpp
@@ -1,29 +1,25 @@
+// Last updated: 8/3/2025, 8:54:30 PM
 class Solution {
 public:
-    void backtrack(vector<int>& candidates, int target, vector<vector<int>>& res, vector<int>& current,int start ){
-        //base case:
+    void backtrack(int start, vector<int>& candidates, int target, vector<int> curr,vector<vector<int>>& res ){
+        int n= candidates.size();
         if(target==0){
-            res.push_back(current);
+            res.push_back(curr);
             return;
         }
-        for(int i=start;i<candidates.size();i++){
-            if(candidates[i]>target){
-                continue;
-            }
-            if(i>start && candidates[i]==candidates[i-1])
-            continue;
-            current.push_back(candidates[i]);
-            backtrack(candidates, target-candidates[i], res, current,i+1);
-            current.pop_back();
+        for(int i=start;i<n;i++){
+            if(i>start&& candidates[i]==candidates[i-1]) continue;
+            if(target<candidates[i]) break;
+            curr.push_back(candidates[i]);
+            backtrack(i+1,candidates,target-candidates[i],curr,res);
+            curr.pop_back();
         }
-
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
+        sort(candidates.begin(), candidates.end());
+        vector<int> curr;
         vector<vector<int>> res;
-        vector<int> current;
-        backtrack(candidates, target, res, current, 0);
+        backtrack(0,candidates, target, curr, res);
         return res;
     }
-
 };
