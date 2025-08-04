e842d4b9b6984408cd73441b8e3fed8b38da10ec
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-08-05 00:30:28 +0530

    Create 1980-FindUniqueBinaryString

---
+++ 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.cpp
@@ -0,0 +1,29 @@
+// Last updated: 8/5/2025, 12:30:27 AM
+class Solution {
+public:
+    string ans="";
+    unordered_set<string> st;
+    bool found = false;
+    void solve(vector<string>& nums, int k, string curr){
+        //if(found) return;
+        if(curr.size()==k){
+            
+            if(st.find(curr)==st.end()){
+                found=true;
+                ans=curr;
+                
+            }
+            return;
+        }
+        
+        solve(nums,k,curr+"1");
+        
+        solve(nums,k,curr+"0");
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        int k= nums.size();
+        for(auto num:nums) st.insert(num);
+        solve(nums, k,"");
+        return ans;
+    }
+};
