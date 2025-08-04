975e2030ebf0f6be945a85daa9380e37c0e7625d
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-08-05 00:30:37 +0530

    File updated at 8/5/2025, 12:30:36 AM

--- 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.cpp
+++ 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.cpp
@@ -1,11 +1,11 @@
-// Last updated: 8/5/2025, 12:30:27 AM
+// Last updated: 8/5/2025, 12:30:36 AM
 class Solution {
 public:
     string ans="";
     unordered_set<string> st;
     bool found = false;
     void solve(vector<string>& nums, int k, string curr){
-        //if(found) return;
+        if(found) return;
         if(curr.size()==k){
             
             if(st.find(curr)==st.end()){
