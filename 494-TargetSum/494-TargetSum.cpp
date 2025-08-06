bf08e5d23fe33ef43111ece91b68c3cac3f27db9
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-08-07 00:56:58 +0530

    File updated at 8/7/2025, 12:56:57 AM

--- 494-TargetSum/494-TargetSum.cpp
+++ 494-TargetSum/494-TargetSum.cpp
@@ -1,28 +1,24 @@
-// Last updated: 8/7/2025, 12:55:57 AM
+// Last updated: 8/7/2025, 12:56:57 AM
 class Solution {
 public:
-    int f(vector<int>& nums, int target, int index, vector<vector<int>>& dp,int offset){
-        if (target + offset < 0 || target + offset >= dp[0].size()) return 0;
-
-        if(index==0){
-            if (nums[0] == 0 && target == 0) return 2; // +0 and -0 both valid
-    if (target == nums[0] || target == -nums[0]) return 1;
-    return 0;
-            
+    int f(int index, int target, vector<int>& nums, int current_sum, vector<vector<int>>& dp, int offset){
+        if(index<0){
+            if(current_sum==target) return 1;
+            else return 0;
         }
-        if(dp[index][target+offset]!=-1) return dp[index][target+offset];
-        int neg=f(nums, target-nums[index],index-1,dp,offset);
-        int pos=f(nums,target+nums[index],index-1,dp,offset);
-        return dp[index][target+offset]=neg+pos;
+        if(dp[index][offset+current_sum]!=-1) return dp[index][offset+current_sum];
+        int neg= f(index-1, target, nums, current_sum -nums[index], dp, offset);
+        int pos = f(index-1, target, nums, current_sum +nums[index], dp, offset);
+        return dp[index][offset+current_sum]=neg+pos;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        int offset=0;
-        int sum=0;
-        for(int i=0;i<n;i++) sum+=nums[i];
-        if (abs(target) > sum) return 0;
-        offset=sum;
-        vector<vector<int>> dp(n,vector<int>(2*sum+1,-1));
-        return f(nums,target,n-1,dp,offset);
+        int n= nums.size();
+        int tot=0;
+        for(int i=0;i<n;i++){
+            tot+=nums[i];
+        }
+        int offset= tot;
+        vector<vector<int>> dp(n, vector<int>(2*tot+1,-1));
+        return f(n-1, target, nums, 0 ,dp, offset);
     }
 };
