4874cf263ab64b66413d75a2b4f419de7129a5bd
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-08-11 00:33:48 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.cpp
@@ -0,0 +1,12 @@
+// Last updated: 8/11/2025, 12:33:47 AM
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0) sum+=i;
+        }
+        if(sum==num) return true;
+        return false;
+    }
+};
