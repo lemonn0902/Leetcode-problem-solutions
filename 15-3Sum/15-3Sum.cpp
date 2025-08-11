d266066bb0c98bfc59e174d2b2ec02204ce3f6c5
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-08-11 12:34:25 +0530

    File updated at 8/11/2025, 12:34:25 PM

--- 15-3Sum/15-3Sum.cpp
+++ 15-3Sum/15-3Sum.cpp
@@ -1,31 +1,27 @@
-// Last updated: 8/11/2025, 12:33:49 PM
+// Last updated: 8/11/2025, 12:34:25 PM
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        for(int i=0;i<nums.size();i++){
-            if(i>0&& nums[i]==nums[i-1]) continue;
-            int j=i+1;
-            int k=n-1;
-            while(j<k){
-                int sum= nums[i]+nums[j]+nums[k];
-            if(sum>0){
-                k--;
+        for(int i=0;i<n;i++){
+            if(i>0&&nums[i]==nums[i-1]) continue;
+            int l=i+1;
+            int r=n-1;
+            while(l<r){
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum==0){
+                    res.push_back({nums[i],nums[l],nums[r]});
+                    l++;
+                    r--;
+                    while(l<r&& nums[l]==nums[l-1]) l++;
+                    while(l<r&& nums[r]==nums[r+1]) r--;
+
+                } 
+                else if(sum<0) l++;
+                else r--;
             }
-            else if(sum<0){
-                j++;
-            }
-            else{
-                res.push_back({nums[i],nums[j],nums[k]});
-                j++;
-                k--;
-                while (j < k && nums[j] == nums[j - 1]) j++;
-                while (j < k && nums[k] == nums[k + 1]) k--;
-            }
-            }
-            
         }
         return res;
     }
