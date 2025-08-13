1ef18bc0a686f3873bf433b37722ce2263838f82
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-08-13 19:15:56 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.cpp
@@ -0,0 +1,25 @@
+// Last updated: 8/13/2025, 7:15:56 PM
+class Solution {
+public:
+    int countBin(int no){
+        if(no==0) return 0;
+        if(no==1) return 1;
+        int res=0;
+        string bin="";
+        while(no!=0){
+            int r=no%2;
+            if(r==1) res++;
+            bin=to_string(r)+bin;
+            no=no/2;
+        }
+        return res;
+    }
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i=0;i<=n;i++){
+            int c=countBin(i);
+            ans.push_back(c);
+        }
+        return ans;
+    }
+};
