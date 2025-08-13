0109dea27200447896d7069a7f851bab3b3fde4a
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-08-13 19:16:47 +0530

    File updated at 8/13/2025, 7:16:46 PM

--- 338-CountingBits/338-CountingBits.cpp
+++ 338-CountingBits/338-CountingBits.cpp
@@ -1,15 +1,14 @@
-// Last updated: 8/13/2025, 7:16:14 PM
+// Last updated: 8/13/2025, 7:16:46 PM
 class Solution {
 public:
     int countBin(int no){
         if(no==0) return 0;
         if(no==1) return 1;
         int res=0;
-        string bin="";
+        
         while(no!=0){
             int r=no%2;
             if(r==1) res++;
-            bin=to_string(r)+bin;
             no=no/2;
         }
         return res;
