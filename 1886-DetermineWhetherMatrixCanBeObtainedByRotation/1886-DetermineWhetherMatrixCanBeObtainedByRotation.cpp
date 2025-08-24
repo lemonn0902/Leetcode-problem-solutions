a5b921753587e4bd4e9c400ec07875e6cde236bb
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-08-25 00:58:46 +0530

    Create 1886-DetermineWhetherMatrixCanBeObtainedByRotation

---
+++ 1886-DetermineWhetherMatrixCanBeObtainedByRotation/1886-DetermineWhetherMatrixCanBeObtainedByRotation.cpp
@@ -0,0 +1,23 @@
+// Last updated: 8/25/2025, 12:58:46 AM
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat){
+        int n=mat.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(mat[i][j], mat[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(mat[i].begin(), mat[i].end());
+        }
+    }
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        int n=mat.size();
+        for(int i=0;i<4;i++){
+            rotate(mat);
+            if(mat==target) return true;
+        }
+        return false;
+    }
+};
