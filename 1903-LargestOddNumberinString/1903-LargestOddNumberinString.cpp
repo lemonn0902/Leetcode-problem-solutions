6770d098ee2243baf06b1e4077eb2df03bdd9ba8
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-08-28 23:53:09 +0530

    File updated at 8/28/2025, 11:53:09 PM

--- 1903-LargestOddNumberinString/1903-LargestOddNumberinString.cpp
+++ 1903-LargestOddNumberinString/1903-LargestOddNumberinString.cpp
@@ -1,18 +1,15 @@
-// Last updated: 8/28/2025, 11:48:29 PM
+// Last updated: 8/28/2025, 11:53:09 PM
 class Solution {
 public:
     string largestOddNumber(string num) {
-       
-         int index=0; int c=0;
-        for(int i=0;i<num.length();i++){
+        int idx=0, c=0;
+        for(int i=0;i<num.size();i++){
             if((num[i]-'0')%2!=0){
-                
-                index=i;
+                idx=i;
                 c++;
-            } 
+            }
         }
-        if(c>0)
-        return num.substr(0,index+1);
+        if(c>0) return num.substr(0,idx+1);
         else return "";
     }
 };
