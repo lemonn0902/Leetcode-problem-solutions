898e21ec519fd49e51be38b0fe363309aa058567
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-09-01 00:34:09 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/205-IsomorphicStrings.cpp
@@ -0,0 +1,24 @@
+// Last updated: 9/1/2025, 12:34:08 AM
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, int> charIndexS;
+        unordered_map<char, int> charIndexT;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (charIndexS.find(s[i]) == charIndexS.end()) {
+                charIndexS[s[i]] = i;
+            }
+
+            if (charIndexT.find(t[i]) == charIndexT.end()) {
+                charIndexT[t[i]] = i;
+            }
+
+            if (charIndexS[s[i]] != charIndexT[t[i]]) {
+                return false;
+            }
+        }
+
+        return true;        
+    }
+};
