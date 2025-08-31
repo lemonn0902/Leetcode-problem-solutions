fed25f11758ebadbff47d5abe240aec8ac32d45a
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-09-01 00:37:29 +0530

    File updated at 9/1/2025, 12:37:29 AM

--- 205-IsomorphicStrings/205-IsomorphicStrings.cpp
+++ 205-IsomorphicStrings/205-IsomorphicStrings.cpp
@@ -1,24 +1,25 @@
-// Last updated: 9/1/2025, 12:34:08 AM
+// Last updated: 9/1/2025, 12:37:29 AM
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, int> charIndexS;
-        unordered_map<char, int> charIndexT;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (charIndexS.find(s[i]) == charIndexS.end()) {
-                charIndexS[s[i]] = i;
+        unordered_map<char,char> mpp;
+        unordered_set<char> used;
+        int m=s.size();
+        int n=t.size();
+        if(m!=n) return false;
+        for(int i=0;i<m;i++){
+            if(mpp.find(s[i])==mpp.end()){
+                if (used.count(t[i])) return false;
+                mpp[s[i]]=t[i];
+                used.insert(t[i]);
             }
+            else{
+                char should=t[i];
+                char is=mpp[s[i]];
+                if(should!=is) return false;
 
-            if (charIndexT.find(t[i]) == charIndexT.end()) {
-                charIndexT[t[i]] = i;
-            }
-
-            if (charIndexS[s[i]] != charIndexT[t[i]]) {
-                return false;
             }
         }
-
-        return true;        
+        return true;
     }
 };
