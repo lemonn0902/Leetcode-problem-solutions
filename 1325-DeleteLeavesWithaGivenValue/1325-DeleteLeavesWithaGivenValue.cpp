3c8f299d9966b1b6c0eae1ac91f2993bfd323c00
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-09-04 13:18:18 +0530

    Create 1325-DeleteLeavesWithaGivenValue

---
+++ 1325-DeleteLeavesWithaGivenValue/1325-DeleteLeavesWithaGivenValue.cpp
@@ -0,0 +1,23 @@
+// Last updated: 9/4/2025, 1:18:17 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if(!root) return NULL;
+        root->left=removeLeafNodes(root->left,target);
+        root->right=removeLeafNodes(root->right,target);
+        if(!root->left&&!root->right&&root->val==target)
+        root=NULL;
+        return root;
+    }
+};
