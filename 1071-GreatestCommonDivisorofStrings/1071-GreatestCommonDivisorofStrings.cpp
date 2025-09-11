b7a8817372f077d921aa565dedb23cdb357a28a8
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-09-11 16:31:50 +0530

    Create 1071-GreatestCommonDivisorofStrings

---
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.cpp
@@ -0,0 +1,9 @@
+// Last updated: 9/11/2025, 4:31:50 PM
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2!=str2+str1) return "";
+        int gcdLen=gcd(str1.size(),str2.size());
+        return str1.substr(0,gcdLen);
+    }
+};
