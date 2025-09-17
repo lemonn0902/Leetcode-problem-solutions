70dcec93593dbc0c0580eb10c03ff8884ddb1ab5
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-09-18 01:16:04 +0530

    File updated at 9/18/2025, 1:16:04 AM

--- 46-Permutations/46-Permutations.cpp
+++ 46-Permutations/46-Permutations.cpp
@@ -1,29 +1,32 @@
-// Last updated: 8/3/2025, 12:33:31 AM
+// Last updated: 9/18/2025, 1:16:04 AM
 class Solution {
 public:
-    void backtrack(vector<int>& nums, vector<int> perm,vector<vector<int>>& res, vector<bool> used){
-        int n=nums.size();
-        //base case
-        if(perm.size()==n){
-            res.push_back(perm);
+    void backtrack(vector<int>& nums, vector<int>& curr, vector<vector<int>> &res, int n, vector<bool>& vis){
+        
+        if(curr.size()==n){
+            res.push_back(curr);
             return;
         }
         for(int i=0;i<n;i++){
-            if(used[i]) continue;
-            perm.push_back(nums[i]);
-            used[i]=true;
-            backtrack(nums, perm, res,used);
-            perm.pop_back();
-            used[i]=false;
+            if(vis[i]==false){
+                curr.push_back(nums[i]);
+            vis[i]=true;
+            backtrack(nums, curr, res,n,vis);
+            curr.pop_back();
+            vis[i]=false;
+            }
+            
+            
         }
+
 
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> perm;
-        vector<vector<int>> res;
-        int n=nums.size();
-        vector<bool> used(n, false);
-        backtrack(nums, perm,res, used);
-        return res;
+       vector<int> curr;
+       vector<vector<int>> res;
+       int n=nums.size();
+       vector<bool> vis(n,false);
+       backtrack(nums, curr, res,n, vis);
+       return res; 
     }
 };
