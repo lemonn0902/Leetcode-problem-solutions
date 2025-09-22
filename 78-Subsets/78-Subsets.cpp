8aacf2f22c65859c48bf380521f4d9413184e130
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-09-22 16:05:39 +0530

    File updated at 9/22/2025, 4:05:38 PM

--- 78-Subsets/78-Subsets.cpp
+++ 78-Subsets/78-Subsets.cpp
@@ -1,23 +1,21 @@
-// Last updated: 4/18/2025, 12:14:44 AM
+// Last updated: 9/22/2025, 4:05:38 PM
 class Solution {
 public:
-    void getSubsets(vector<int>& nums, int index,int n, vector<vector<int>> &res, vector<int>& subsets){
-        if(index==n)
-        {
-            res.push_back(subsets);
+    void backtrack(vector<int>& nums, vector<vector<int>>& res, vector<int>& sub, int i){
+        int n=nums.size();
+        if(i==n){
+            res.push_back(sub);
             return;
         }
-        subsets.push_back(nums[index]);
-        getSubsets(nums, index+1, n, res, subsets);
-        subsets.pop_back();
-        getSubsets(nums, index+1, n, res, subsets);
-
+        sub.push_back(nums[i]);
+        backtrack(nums, res, sub,i+1);
+        sub.pop_back();
+        backtrack(nums, res, sub,i+1);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
-        int n=nums.size();
-        vector<int> subsets;
-        getSubsets(nums, 0,n, res, subsets);
+        vector<int> sub;
+        backtrack(nums, res, sub,0);
         return res;
     }
 };
