b15019ff541b696c8be4d236a09aa86c41a6cd62
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-09-22 17:17:45 +0530

    File updated at 9/22/2025, 5:17:45 PM

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.cpp
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.cpp
@@ -1,40 +1,36 @@
-// Last updated: 8/4/2025, 4:39:44 PM
+// Last updated: 9/22/2025, 5:17:45 PM
 class Solution {
 public:
-    unordered_map<char, string> map={
-        {'2',"abc"},
-        {'3',"def"},
-        {'4',"ghi"},
-        {'5',"jkl"},
-        {'6',"mno"},
-        {'7',"pqrs"},
-        {'8',"tuv"},
-        {'9',"wxyz"},
+    unordered_map<char,string> mpp={
+        {'2', "abc"},
+        {'3', "def"},
+        {'4', "ghi"},
+        {'5', "jkl"},
+        {'6', "mno"},
+        {'7', "pqrs"},
+        {'8', "tuv"},
+        {'9', "wxyz"},
         
     };
-    void backtrack(string digits, vector<string>& res,int index, string curr){
-        int n=digits.size();
+    void backtrack(string digits,vector<string>& res,string curr, int index, int n ){
         if(curr.size()==n){
             res.push_back(curr);
             return;
         }
-        char dig=digits[index]; 
-        const string& choices = map[dig];
+        char dig=digits[index];
+        string choices= mpp[dig];
         for(char ch:choices){
-            
-            
-            
-                curr.push_back(ch);
-                backtrack(digits,res,index+1,curr);
-                curr.pop_back();
-            }
-        
+            curr.push_back(ch);
+            backtrack(digits, res, curr, index+1, n);
+            curr.pop_back();
+        }
 
     }
     vector<string> letterCombinations(string digits) {
         if(digits=="") return {};
         vector<string> res;
-        backtrack(digits, res,0,"");
+        int n= digits.size();
+        backtrack(digits, res, "",0,n);
         return res;
     }
 };
