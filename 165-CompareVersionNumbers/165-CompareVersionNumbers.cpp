46b379ef7fa4f898edf2d3facec90a15fb2aac31
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-09-23 12:55:51 +0530

    Create 165-CompareVersionNumbers

---
+++ 165-CompareVersionNumbers/165-CompareVersionNumbers.cpp
@@ -0,0 +1,28 @@
+// Last updated: 9/23/2025, 12:55:50 PM
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+       int n=version1.size();
+       int m=version2.size();
+       int i=0, j=0;
+       while(i<n|| j<m){
+            string n1="", n2="";
+            while( i<n&&version1[i]!='.' ){
+                n1=n1+version1[i];
+                i++;
+            } 
+            i++;
+            while(j<m&&version2[j]!='.' ){
+                n2=n2+version2[j];
+                j++;
+            } 
+            j++;
+            int v1 = n1.empty() ? 0 : stoi(n1);
+            int v2 = n2.empty() ? 0 : stoi(n2);
+            if (v1 > v2) return 1;
+            if (v1 < v2) return -1;
+       }
+       
+       return 0;
+    }
+};
