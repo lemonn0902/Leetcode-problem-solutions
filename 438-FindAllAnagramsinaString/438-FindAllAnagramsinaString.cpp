3e2c89937bdfa6a95ade2461a000ae3aef04d7cd
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-10-12 14:56:38 +0530

    File updated at 10/12/2025, 2:56:38 PM

--- 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.cpp
+++ 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.cpp
@@ -1,24 +1,27 @@
-// Last updated: 7/28/2025, 1:41:08 PM
+// Last updated: 10/12/2025, 2:56:38 PM
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         vector<int> res;
-        int windowSize=p.size();
-        int n=s.size();
-        if(windowSize>n) return {};
+        unordered_map<char,int> freq;
+        unordered_map<char,int> mpp;
+        for(char c:p){
+            freq[c]++;
+        }
+        int win_size=p.size();
         int l=0;
         int r=0;
-        unordered_map<char,int> first;
-        for(int i=0;i<windowSize;i++) first[p[i]]++;
-        unordered_map<char,int> second;
+        int n=s.size();
         while(r<n){
-            second[s[r]]++;
-            while((1+r-l)>windowSize){
-                second[s[l]]--;
-                if(second[s[l]]==0) second.erase(s[l]);
+            mpp[s[r]]++;
+            if(1+r-l>win_size){
+                mpp[s[l]]--;
+                if(mpp[s[l]]==0) mpp.erase(s[l]);
                 l++;
             }
-            if((1+r-l==windowSize)&&second==first) res.push_back(l);
+            if(1+r-l==win_size&& freq==mpp){
+                res.push_back(l);
+            }
             r++;
         }
         return res;
