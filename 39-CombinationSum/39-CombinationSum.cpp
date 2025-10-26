27f7e844008e3b6c9086a0e80f3875b8158b18df
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-10-26 15:18:07 +0530

    File updated at 10/26/2025, 3:18:06 PM

--- 39-CombinationSum/39-CombinationSum.cpp
+++ 39-CombinationSum/39-CombinationSum.cpp
@@ -1,21 +1,23 @@
-// Last updated: 10/26/2025, 2:49:02 PM
+// Last updated: 10/26/2025, 3:18:06 PM
 class Solution {
 public:
     void backtrack(vector<int>& candidates, int target, vector<vector<int>>& res, vector<int>& curr , int index){
-        if(target==0){
-            
-            res.push_back(curr);
+        int n=candidates.size();
+        if(index==n){
+            if(target==0){
+                res.push_back(curr);
+                
+            }
             return;
         }
-        for(int i=index;i<candidates.size();i++){
-            if(candidates[i]> target) break;
-            curr.push_back(candidates[i]);
-            backtrack(candidates, target-candidates[i], res, curr, i);
+        if(candidates[index]<=target){
+            curr.push_back(candidates[index]);
+            backtrack(candidates,target-candidates[index],res,curr,index);
             curr.pop_back();
         }
+        backtrack(candidates, target,res, curr, index+1);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
         vector<vector<int>> res;
         vector<int> curr;
         backtrack(candidates, target, res, curr, 0);
