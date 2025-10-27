2ad61738b453c986cafc696e87c554c91318951b
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-10-27 17:16:49 +0530

    File updated at 10/27/2025, 5:16:49 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
@@ -1,26 +1,26 @@
-// Last updated: 10/27/2025, 5:16:11 PM
+// Last updated: 10/27/2025, 5:16:49 PM
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low=0;
+        int low= 0;
         int high=nums.size()-1;
         while(low<=high){
             int mid=low+(high-low)/2;
             if(nums[mid]==target) return mid;
-            else if(nums[low]<=nums[mid]){
-                if(nums[mid]>target && target>=nums[low]){
+            if(nums[mid]>=nums[low]){
+                //left part is sorted
+                if(target>=nums[low]&&target<=nums[mid]){
                     high=mid-1;
-
                 }
                 else low=mid+1;
             }
             else{
-                if(target>nums[mid]&& target<=nums[high]){
+                //right part is sorted 
+                if(target>=nums[mid]&&target<=nums[high]){
                     low=mid+1;
                 }
-                else high=mid-1;
+                else high=mid-1; 
             }
-
         }
         return -1;
     }
