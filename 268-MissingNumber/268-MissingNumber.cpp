284679aa93e0ff5890dc955a59d2055d67991e3f
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-11-11 22:37:34 +0530

    File updated at 11/11/2025, 10:37:34 PM

--- 268-MissingNumber/268-MissingNumber.cpp
+++ 268-MissingNumber/268-MissingNumber.cpp
@@ -1,11 +1,12 @@
-// Last updated: 7/29/2025, 12:39:30 PM
+// Last updated: 11/11/2025, 10:37:34 PM
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n= nums.size();
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
+        int n=nums.size();
         for(int i=0;i<n;i++){
-            if(nums[i]!=i) return i;
+            if(nums[i]!=i)
+            return i;
         }
         return n;
     }
