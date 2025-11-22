184116eaa62e4085ffd4cbab9b51a46ddc5b713e
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-11-22 14:41:54 +0530

    File updated at 11/22/2025, 2:41:54 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
@@ -1,16 +1,19 @@
-// Last updated: 11/22/2025, 12:45:54 PM
+// Last updated: 11/22/2025, 2:41:54 PM
 class Solution {
 public:
-    vector<int> getConcatenation(vector<int>& nums) {
-        int n=nums.size();
+    vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> ans(2*n,1);
-        for(int i=0;i<n;i++){
-            ans[i]=nums[i];
-        }
-        int j=0;
-        for(int i=n;i<2*n;i++){
-            ans[i]=nums[j];
-            j++;
+        int j=n;
+        int k=0;
+        for(int i=0;i<2*n;i++){
+            if(i%2!=0){
+                ans[i]=nums[j];
+                j++;
+            }
+            else{
+                ans[i]=nums[k];
+                k++;
+            }
         }
         return ans;
     }
