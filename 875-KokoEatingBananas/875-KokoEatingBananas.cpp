5718a80eef79a85b8fb419e36d7acc82f62739b7
lemon <155637262+lemonn0902@users.noreply.github.com>
2025-11-22 22:21:34 +0530

    File updated at 11/22/2025, 10:21:34 PM

--- 875-KokoEatingBananas/875-KokoEatingBananas.cpp
+++ 875-KokoEatingBananas/875-KokoEatingBananas.cpp
@@ -1,29 +1,30 @@
-// Last updated: 10/29/2025, 12:19:04 AM
+// Last updated: 11/22/2025, 10:21:34 PM
 class Solution {
 public:
     long long find(vector<int>& piles, int per){
         long long ans=0;
         for(int i=0;i<piles.size();i++){
-            ans += ceil((double)piles[i] / per);
+            ans+= ceil((double)piles[i]/per);
         }
         return ans;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
         int maxi=INT_MIN;
-        for(int n:piles){
-            if(n>maxi) maxi= n;
+        for(int x:piles){
+            if(x>maxi) maxi=x;
         }
-        int l=1;
-        int e=maxi;
-        while(l<=e){
-            int m=l+(e-l)/2;
-            long long hrs=find(piles, m);
+        int low=1;
+        int high=maxi;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long long hrs=find(piles,mid);
             if(hrs>h){
-                l=m+1;
+                low=mid+1;
             }
-            else e=m-1;
+            else{
+                high=mid-1;
+            }
         }
-        return l;
-
+        return low;
     }
 };
