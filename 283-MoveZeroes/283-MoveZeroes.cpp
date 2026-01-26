87b973ef4a6a4cd5d487ee8341173b8c78806ba2
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-01-26 23:32:21 +0530

    File updated at 1/26/2026, 11:32:21 PM

--- 283-MoveZeroes/283-MoveZeroes.cpp
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -1,17 +1,14 @@
-// Last updated: 1/26/2026, 11:26:13 PM
+// Last updated: 1/26/2026, 11:32:21 PM
 1class Solution {
 2public:
 3    void moveZeroes(vector<int>& nums) {
-4        int lastNonZeroFoundAt=0;
-5        for(int i =0;i<nums.size();i++){
-6            if(nums[i]!=0){
-7                nums[lastNonZeroFoundAt]=nums[i];
-8                lastNonZeroFoundAt++;
-9            }
-10
+4        int idx=0;
+5        int n=nums.size();
+6        for(int i=0;i<n;i++){
+7            if(nums[i]!=0){
+8                swap(nums[idx], nums[i]);
+9                idx++;
+10            }
 11        }
-12        for(int i=lastNonZeroFoundAt;i<nums.size();i++){
-13            nums[i]=0;
-14        }
-15    }
-16};
+12    }
+13};
