b24c2440d1bbcba5d8058fe0cf073e7689e10eca
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-01-27 15:45:07 +0530

    File updated at 1/27/2026, 3:45:06 PM

--- 75-SortColors/75-SortColors.cpp
+++ 75-SortColors/75-SortColors.cpp
@@ -1,25 +1,25 @@
-// Last updated: 9/13/2025, 1:21:05 PM
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        int high=nums.size()-1;
-        int low=0;
-        int mid= 0;
-        while(mid<=high){
-            if(nums[mid]==0){
-                swap(nums[mid], nums[low]);
-                low++;
-                mid++;
-            }
-            else if(nums[mid]==1){
-                mid++;
-            }
-            else if(nums[mid]==2){
-                swap(nums[mid], nums[high]);
-                high--;
-
-            }
-
-        }
-    }
-};
+// Last updated: 1/27/2026, 3:45:06 PM
+1class Solution {
+2public:
+3    void sortColors(vector<int>& nums) {
+4        int high=nums.size()-1;
+5        int low=0;
+6        int mid=0;
+7        while(mid<=high){
+8            
+9            if(nums[mid]==1){
+10                mid++;
+11            }
+12            else if(nums[mid]==0){
+13                swap(nums[mid], nums[low]);
+14                low++;
+15                mid++;
+16            }
+17            else if(nums[mid]==2){
+18                swap(nums[mid], nums[high]);
+19                high--;
+20                
+21            }
+22        }
+23    }
+24};
