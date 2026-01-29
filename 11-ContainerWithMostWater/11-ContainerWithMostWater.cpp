fc4f3f82a2039acb5ac0cd07f9c7872df5c92f2b
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-01-29 21:57:54 +0530

    File updated at 1/29/2026, 9:57:54 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
@@ -1,23 +1,20 @@
-// Last updated: 7/23/2025, 10:26:27 PM
-class Solution {
-public:
-    int maxArea(vector<int>& height) {
-        int maxArea=0;
-        int n= height.size();
-        int l=0;
-        int r=n-1;
-        for(int i=0;i<n;i++){
-            int len=min(height[l],height[r]);
-            int width=r-l;
-            int area= len* width;
-            maxArea=max(maxArea,area);
-            if(height[l]<height[r]){
-                l++;
-            }
-            else{
-                r--;
-            }
-        }
-        return maxArea;
-    }
-};
+// Last updated: 1/29/2026, 9:57:54 PM
+1class Solution {
+2public:
+3    int maxArea(vector<int>& height) {
+4        int ans=0;
+5        int r=height.size()-1;
+6        int l=0;
+7        for(int i=0;i<height.size();i++){
+8            int length= min(height[l], height[r]);
+9            int width= (r-l);
+10            int area= length* width;
+11            ans=max(ans, area);
+12            if(height[l]<height[r]){
+13                l++;
+14            }
+15            else r--;
+16        }
+17        return ans;
+18    }
+19};
