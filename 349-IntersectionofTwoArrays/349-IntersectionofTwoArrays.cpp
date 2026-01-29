2aa6e208e9bb42b9e08a299079aef753a6752ccb
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-01-29 22:39:34 +0530

    File updated at 1/29/2026, 10:39:33 PM

--- 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.cpp
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.cpp
@@ -1,16 +1,30 @@
-// Last updated: 9/6/2025, 10:31:36 AM
-class Solution {
-public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> st(nums1.begin(), nums1.end());
-        unordered_set<int> res;
-        for(int n: nums2){
-            if(st.count(n)){
-                res.insert(n);
-            }
-        }
-        vector<int> ans(res.begin(), res.end());
-        return ans;
-
-    }
-};
+// Last updated: 1/29/2026, 10:39:33 PM
+1class Solution {
+2public:
+3    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+4        vector<int> res;
+5        sort(nums1.begin(), nums1.end());
+6        sort(nums2.begin(), nums2.end());
+7        int m=nums1.size();
+8        int n=nums2.size();
+9        int i=0;
+10        int j=0;
+11        while(i<m && j<n){
+12            if(nums1[i]==nums2[j]){
+13                if(res.empty()||res.back()!=nums1[i]){
+14                    res.push_back(nums1[i]);
+15                }
+16                    i++;
+17                    j++;
+18                }
+19                else if(nums1[i]>nums2[j]){
+20                    j++;
+21                }
+22                else if(nums1[i]<nums2[j]){
+23                    i++;
+24                }
+25            }
+26        
+27        return res;
+28    }
+29};
