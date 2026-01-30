744752ad3d127aa1ffa1d5f36ef5c08bdebad2ec
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-01-30 12:54:56 +0530

    File updated at 1/30/2026, 12:54:55 PM

--- 202-HappyNumber/202-HappyNumber.cpp
+++ 202-HappyNumber/202-HappyNumber.cpp
@@ -1,22 +1,27 @@
-// Last updated: 9/9/2025, 10:42:20 PM
-class Solution {
-public:
-    int sq(int n){
-        int sum=0;
-        while(n!=0){
-            int r=n%10;
-            sum=sum+(r*r);
-            n=n/10;
-        }
-        return sum;
-    }
-    bool isHappy(int n) {
-        int slow=n;
-        int fast=n;
-        do{
-            slow=sq(slow);
-            fast=sq(sq(fast));
-        } while(slow!=fast);
-        return slow==1;
-    }
-};
+// Last updated: 1/30/2026, 12:54:55 PM
+1class Solution {
+2public:
+3    int square(int n){
+4        int ans=0;
+5        while(n!=0){
+6            int r=n%10;
+7            int sq=r*r;
+8            ans=ans+sq;
+9            n=n/10;
+10        }
+11        return ans;
+12    }
+13    bool isHappy(int n) {
+14        int slow=n;
+15        int fast=n;
+16        while(true){
+17            slow=square(slow);
+18            fast=square(square(fast));
+19            if(slow==fast) {
+20                if(slow==1) return true;
+21                break;
+22            }
+23        }
+24        return false;
+25    }
+26};
