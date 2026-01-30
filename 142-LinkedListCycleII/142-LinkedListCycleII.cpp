c121a0b2731a2a2987adb8f9a076ca4779bb2cf4
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-01-30 12:10:36 +0530

    File updated at 1/30/2026, 12:10:36 PM

--- 142-LinkedListCycleII/142-LinkedListCycleII.cpp
+++ 142-LinkedListCycleII/142-LinkedListCycleII.cpp
@@ -1,31 +1,30 @@
-// Last updated: 7/27/2025, 1:21:36 PM
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
-    ListNode *detectCycle(ListNode *head) {
-        
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast!=NULL && fast->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(slow==fast) {
-                fast=head;
-                while(slow!=fast){
-                    fast=fast->next;
-                    slow=slow->next;
-                }
-                return slow;
-            }
-        }
-        
-        return NULL;
-    }
-};
+// Last updated: 1/30/2026, 12:10:36 PM
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode(int x) : val(x), next(NULL) {}
+7 * };
+8 */
+9class Solution {
+10public:
+11    ListNode *detectCycle(ListNode *head) {
+12        ListNode* slow=head;
+13        ListNode* fast=head;
+14        while(fast && fast->next!=NULL){
+15            slow=slow->next;
+16            fast=fast->next->next;
+17            if(slow==fast){
+18                fast=head;
+19                while(slow!=fast){
+20                    fast=fast->next;
+21                    slow=slow->next;
+22                
+23                }
+24                return slow;
+25            }
+26        }
+27        return nullptr;
+28    }
+29};
