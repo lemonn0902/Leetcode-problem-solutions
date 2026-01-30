e2bef682f4e753a4502f57bdca77a42466ee35cd
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-01-30 13:22:17 +0530

    File updated at 1/30/2026, 1:22:16 PM

--- 392-IsSubsequence/392-IsSubsequence.cpp
+++ 392-IsSubsequence/392-IsSubsequence.cpp
@@ -1,15 +1,20 @@
-// Last updated: 10/29/2025, 9:06:35 PM
-class Solution {
-public:
-    bool isSubsequence(string s, string t) {
-       
-        int idx=0;
-        for(int i=0;i<t.size()&&idx<s.size();i++){
-            if(s[idx]==t[i]){
-                idx++;
-                continue;
-            }
-        }
-        return idx==s.size();
-    }
-};
+// Last updated: 1/30/2026, 1:22:16 PM
+1class Solution {
+2public:
+3    bool isSubsequence(string s, string t) {
+4        int m=s.size();
+5        int n=t.size();
+6        int i=0;
+7        int j=0;
+8        while(i<m && j<n){
+9            if(s[i]==t[j]){
+10                i++;
+11                j++;
+12            }
+13            else{
+14                j++;
+15            }
+16        }
+17        return i==s.size();
+18    }
+19};
