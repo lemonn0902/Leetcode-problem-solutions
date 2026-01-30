604e214d004b83f6cfd7d67c020da46d6d072eff
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-01-30 11:32:45 +0530

    File updated at 1/30/2026, 11:32:45 AM

--- 881-BoatstoSavePeople/881-BoatstoSavePeople.cpp
+++ 881-BoatstoSavePeople/881-BoatstoSavePeople.cpp
@@ -1,27 +1,24 @@
-// Last updated: 8/11/2025, 2:08:39 PM
-class Solution {
-public:
-    int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(),people.end());
-        int n=people.size();
-        int l=0;
-        int r=n-1;
-        int ans=0;
-        while(l<=r){
-            
-            int sum=people[l]+people[r];
-            if(sum>limit) {
-                ans+=1;
-                r--;
-            }
-            else{
-                ans+=1;
-                l++;
-                r--;
-            }
-            
-
-        }
-        return ans;
-    }
-};
+// Last updated: 1/30/2026, 11:32:45 AM
+1class Solution {
+2public:
+3    int numRescueBoats(vector<int>& people, int limit) {
+4        sort(people.begin(), people.end());
+5        int n=people.size();
+6        int l=0;
+7        int r=n-1;
+8        int ans=0;
+9        while(l<=r){
+10            int sum= people[l]+people[r];
+11            if(sum>limit){
+12                ans+=1;
+13                r--;
+14            }
+15            else{
+16                ans+=1;
+17                l++;
+18                r--;
+19            }
+20        }
+21        return ans;
+22    }
+23};
