adbe25b2cc06f1ccb553ca220b65d49a34fc8cf2
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-01-31 12:03:28 +0530

    File updated at 1/31/2026, 12:03:28 PM

--- 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.cpp
+++ 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.cpp
@@ -1,11 +1,11 @@
-// Last updated: 1/31/2026, 12:02:57 PM
+// Last updated: 1/31/2026, 12:03:28 PM
 1class Solution {
 2public:
 3    int removeDuplicates(vector<int>& nums) {
-4        int n= nums.size();
-5        if(n<=2) return n;
-6        int k=2;
-7        for(int i=2;i<n;i++){
+4        int k=2;
+5        int n=nums.size();
+6        if(n<=2) return n;
+7        for(int i=2;i<nums.size();i++){
 8            if(nums[i]!=nums[k-2]){
 9                nums[k]=nums[i];
 10                k++;
