cd3fcf589b38945a43450c77ca07333c46be02b7
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-01-31 17:11:27 +0530

    File updated at 1/31/2026, 5:11:26 PM

--- 647-PalindromicSubstrings/647-PalindromicSubstrings.cpp
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.cpp
@@ -1,4 +1,4 @@
-// Last updated: 1/31/2026, 5:10:41 PM
+// Last updated: 1/31/2026, 5:11:26 PM
 1class Solution {
 2public:
 3    int expand(string str, int left, int right){
@@ -14,9 +14,9 @@
 13    int countSubstrings(string s) {
 14        int total=0;
 15        for(int center=0;center<s.size();center++){
-16            int len1=expand(s, center, center);
-17            int len2=expand(s, center, center+1);
-18            total+=len1+len2;
+16            total+=expand(s, center, center);
+17            total+=expand(s, center, center+1);
+18            
 19        }
 20        return total;
 21    }
