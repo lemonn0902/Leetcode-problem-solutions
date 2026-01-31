00486fbf4c1e34585a7d1641a813f129d794e243
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-01-31 13:33:08 +0530

    Create 2938-SeparateBlackandWhiteBalls

---
+++ 2938-SeparateBlackandWhiteBalls/2938-SeparateBlackandWhiteBalls.cpp
@@ -0,0 +1,15 @@
+// Last updated: 1/31/2026, 1:33:07 PM
+1class Solution {
+2public:
+3    long long minimumSteps(string s) {
+4        long long ans=0;
+5        long long black=0;
+6        int n=s.size();
+7        for(int i=0;i<n;i++){
+8            if(s[i]=='1') black++;
+9            else ans+=black;
+10
+11        }
+12        return ans;
+13    }
+14};
