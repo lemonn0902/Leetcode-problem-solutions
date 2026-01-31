a090d3b6b73676357ae3f2cc248aac9563a03a2e
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-01-31 16:06:57 +0530

    File updated at 1/31/2026, 4:06:57 PM

--- 844-BackspaceStringCompare/844-BackspaceStringCompare.cpp
+++ 844-BackspaceStringCompare/844-BackspaceStringCompare.cpp
@@ -1,33 +1,28 @@
-// Last updated: 1/31/2026, 3:45:55 PM
+// Last updated: 1/31/2026, 4:06:57 PM
 1class Solution {
 2public:
-3    bool backspaceCompare(string s, string t) {
-4        string s1="";
-5        string s2="";
-6        int m=s.size();
-7        int n=t.size();
-8        int i=0; int j=0;
-9        while(i<m){
-10            if(s[i]=='#'){
-11                if(!s1.empty())
-12                s1.pop_back();
-13            }
-14            else{
-15                s1.push_back(s[i]);
-16            }
-17            i++;
-18        }
-19        while(j<n){
-20            if(t[j]=='#'){
-21                if(!s2.empty())
-22                s2.pop_back();
-23            }
-24            else {
-25                s2.push_back(t[j]);
-26            }
-27            j++;
-28        }
-29        return s1==s2;
-30
-31    }
-32};
+3    int getValidChar(string str, int idx){
+4        int back=0;
+5        while(idx>=0){
+6            if(str[idx]=='#') back++;
+7            else if(back>0) back--;
+8            else break;
+9            idx--;
+10        }
+11        return idx;
+12    }
+13    bool backspaceCompare(string s, string t) {
+14        int i=s.size()-1;
+15        int j=t.size()-1;
+16        while(i>=0 || j>=0){
+17            i= getValidChar(s,i);
+18            j=getValidChar(t, j);
+19            if(i<0&&j<0) return true;
+20            else if(i<0||j<0) return false;
+21            else if(s[i]!=t[j]) return false;
+22            i--;
+23            j--;
+24        }
+25        return true;
+26    }
+27};
