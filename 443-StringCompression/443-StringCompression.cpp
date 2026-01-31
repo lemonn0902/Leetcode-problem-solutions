c11ae39daf268e1cb3124cda9cd4bdb4868d33cf
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-01-31 12:29:07 +0530

    File updated at 1/31/2026, 12:29:06 PM

--- 443-StringCompression/443-StringCompression.cpp
+++ 443-StringCompression/443-StringCompression.cpp
@@ -1,28 +1,26 @@
-// Last updated: 7/31/2025, 2:30:59 PM
-class Solution {
-public:
-    int compress(vector<char>& chars) {
-        int index = 0;  // where to write in the array
-        int i = 0;      
-
-        while (i < chars.size()) {
-            char current = chars[i];
-            int count = 0;
-
-            
-            while (i < chars.size() && chars[i] == current) {
-                i++;
-                count++;
-            }
-            chars[index++] = current;
-            if (count > 1) {
-                string num = to_string(count);
-                for (char c : num) {
-                    chars[index++] = c;
-                }
-            }
-        }
-
-        return index;  
-    }
-};
+// Last updated: 1/31/2026, 12:29:06 PM
+1class Solution {
+2public:
+3    int compress(vector<char>& chars) {
+4        int n=chars.size();
+5        if(n==1) return 1;
+6        int idx=0;
+7        int i=0;
+8        while(i<n){
+9            int count=0;
+10            char curr=chars[i];
+11            while(i<n && chars[i]==curr){
+12                count++;
+13                i++;
+14            }
+15            chars[idx++]=curr;
+16            if(count>1){
+17                string num=to_string(count);
+18                for(char c:num){
+19                    chars[idx++]=c;
+20                }
+21            }
+22        }
+23        return idx;
+24    }
+25};
