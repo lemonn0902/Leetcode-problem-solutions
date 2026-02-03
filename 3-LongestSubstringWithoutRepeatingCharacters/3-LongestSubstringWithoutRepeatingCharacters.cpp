96f5b0b1eb081c20564275dfccd498a6ca20bf62
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-02-03 12:19:36 +0530

    File updated at 2/3/2026, 12:19:36 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,25 +1,22 @@
-// Last updated: 7/27/2025, 11:44:47 PM
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        int n= s.size();
-
-        int l=0;
-        int r=0;
-        int maxAns=0;
-        
-        unordered_map<char,int> freq;
-        while(r<n){
-            
-            freq[s[r]]++;
-            while(freq[s[r]]>1){
-                freq[s[l]]--;
-                
-                l++;
-            }
-            maxAns=max(maxAns, 1+r-l);
-            r++;
-        }
-        return maxAns;
-    }
-};
+// Last updated: 2/3/2026, 12:19:36 PM
+1class Solution {
+2public:
+3    int lengthOfLongestSubstring(string s) {
+4         int n=s.size();
+5         int l=0;
+6         int r=0;
+7         int maxi=0;
+8         string x="";
+9         while(r<n){
+10            while(x.find(s[r])!=string::npos){
+11                x.erase(0,1);
+12                l++;
+13                
+14            }
+15            x.push_back(s[r]);
+16            maxi=max(maxi, r-l+1);
+17            r++;
+18         }
+19         return maxi;
+20    }
+21};
