60d91ed88bf221403a2c590e36def539d673705a
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-03-05 16:52:24 +0530

    File updated at 3/5/2026, 4:52:24 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
@@ -1,23 +1,22 @@
-// Last updated: 7/28/2025, 12:03:05 AM
-class Solution {
-public:
-    int characterReplacement(string s, int k) {
-        int l=0;
-        int r=0;
-        int n=s.size();
-        int maxAns=0;
-        int maxFreq=0;
-        unordered_map<char,int> freq;
-        while(r<n){
-            freq[s[r]]++;
-            maxFreq=max(maxFreq, freq[s[r]]);
-            if((1+r-l)-maxFreq>k){
-                freq[s[l]]--;
-                l++;
-            }
-            maxAns=max(maxAns, 1+r-l);
-            r++;
-        }
-        return maxAns;
-    }
-};
+// Last updated: 3/5/2026, 4:52:24 PM
+1class Solution {
+2public:
+3    int characterReplacement(string s, int k) {
+4        unordered_map<char,int> freq;
+5        int maxi=0;
+6        int maxFreq=0;
+7        int l=0, r=0;
+8        int n=s.size();
+9        while(r<n){
+10            freq[s[r]]++;
+11            maxFreq=max(maxFreq, freq[s[r]]);
+12            if(r-l+1 - maxFreq >k){
+13                freq[s[l]]--;
+14                l++;
+15            }
+16            maxi=max(maxi, r-l+1);
+17            r++;
+18        }
+19        return maxi;
+20    }
+21};
