fe1569c0b10986b111fa23b6f854ce41edb033ac
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-03-05 16:28:05 +0530

    File updated at 3/5/2026, 4:28:05 PM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.cpp
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.cpp
@@ -1,12 +1,15 @@
-// Last updated: 3/5/2026, 4:12:23 PM
+// Last updated: 3/5/2026, 4:28:05 PM
 1class Solution {
 2public:
 3    bool containsNearbyDuplicate(vector<int>& nums, int k) {
-4        unordered_map<int, int> mpp;
+4        unordered_set<int> window;
 5        for(int i=0;i<nums.size();i++){
-6            if(mpp.count(nums[i]) && i-mpp[nums[i]]<=k) return true;
-7            mpp[nums[i]]=i;
-8        }
-9        return false;
-10    }
-11};
+6            if(window.count(nums[i])) return true;
+7            window.insert(nums[i]);
+8            if(window.size()>k){
+9                window.erase(nums[i-k]);
+10            }
+11        }
+12        return false;
+13    }
+14};
