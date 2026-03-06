3c1545de667df83bc4041268c8d8f2677fec608d
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-03-06 19:21:41 +0530

    File updated at 3/6/2026, 7:21:41 PM

--- 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.cpp
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.cpp
@@ -1,23 +1,21 @@
-// Last updated: 3/6/2026, 7:20:33 PM
+// Last updated: 3/6/2026, 7:21:41 PM
 1class Solution {
 2public:
 3    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
 4        if(k<=1) return 0;
-5        int l=0;
-6        int r=0;
-7        int n= nums.size();
+5        int l=0, r=0;
+6        int n=nums.size();
+7        int prod=1;
 8        int res=0;
-9        int prod=1;
-10        while(r<n){
-11            
-12            prod=prod*nums[r];
-13            while(prod>=k){
-14                prod=prod/nums[l];
-15                l++;
-16            } 
-17            res+=1+r-l;
-18            r++;
-19        }
-20        return res;
-21    }
-22};
+9        while(r<n){
+10            prod=prod*nums[r];
+11            while(prod>=k){
+12                prod=prod/nums[l];
+13                l++;
+14            }
+15            res+=(r-l+1);
+16            r++;
+17        }
+18        return res;
+19    }
+20};
