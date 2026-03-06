1673446036c1277179bee8538d4e68500105803c
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-03-06 19:52:00 +0530

    File updated at 3/6/2026, 7:51:59 PM

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.cpp
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.cpp
@@ -1,21 +1,26 @@
-// Last updated: 7/28/2025, 12:51:23 PM
-class Solution {
-public:
-    int longestOnes(vector<int>& nums, int k) {
-        int l=0;
-        int r=0;
-        int n=nums.size();
-        int maxAns=0;
-        int zeroCount=0;
-        while(r<n){
-            if(nums[r]==0) zeroCount++;
-            if(zeroCount>k){
-                if(nums[l]==0) zeroCount--;
-                l++;
-            }
-            maxAns=max(maxAns, 1+r-l);
-            r++;
-        }
-        return maxAns;
-    }
-};
+// Last updated: 3/6/2026, 7:51:59 PM
+1class Solution {
+2public:
+3    int longestOnes(vector<int>& nums, int k) {
+4        int ans = 0;
+5        int l = 0, r = 0;
+6        int zeroCount = 0;
+7
+8        while(r < nums.size()){
+9
+10            if(nums[r] == 0)
+11                zeroCount++;
+12
+13            while(zeroCount > k){
+14                if(nums[l] == 0)
+15                    zeroCount--;
+16                l++;
+17            }
+18
+19            ans = max(ans, r - l + 1);
+20            r++;
+21        }
+22
+23        return ans;
+24    }
+25};
