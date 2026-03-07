0176976374aa75fb9cda67eb2a98146439bace5e
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-03-07 11:06:33 +0530

    File updated at 3/7/2026, 11:06:33 AM

--- 1493-LongestSubarrayof1'sAfterDeletingOneElement/1493-LongestSubarrayof1'sAfterDeletingOneElement.cpp
+++ 1493-LongestSubarrayof1'sAfterDeletingOneElement/1493-LongestSubarrayof1'sAfterDeletingOneElement.cpp
@@ -1,24 +1,20 @@
-// Last updated: 8/24/2025, 10:56:50 AM
-class Solution {
-public:
-    int longestSubarray(vector<int>& nums) {
-        int maxAns=0;
-        int l=0;
-        int r=0;
-        int n=nums.size();
-        int no_of_zeroes=0;
-        
-        while(r<n){
-            if(nums[r]==0){
-                no_of_zeroes++;
-            }
-            while(no_of_zeroes>1){
-                if(nums[l]==0) no_of_zeroes--;
-                l++;
-            }
-            maxAns=max(maxAns,1+r-l);
-            r++;
-        }
-        return --maxAns;
-    }
-};
+// Last updated: 3/7/2026, 11:06:33 AM
+1class Solution {
+2public:
+3    int longestSubarray(vector<int>& nums) {
+4        int zeroCount=0;
+5        int l=0, r=0;
+6        int n=nums.size();
+7        int maxi=0;
+8        while(r<n){
+9            if(nums[r]==0) zeroCount++;
+10            while(zeroCount>1){
+11                if(nums[l]==0) zeroCount--;
+12                l++;
+13            }
+14            maxi=max(maxi, r-l+1);
+15            r++;
+16        }
+17        return maxi-1;
+18    }
+19};
