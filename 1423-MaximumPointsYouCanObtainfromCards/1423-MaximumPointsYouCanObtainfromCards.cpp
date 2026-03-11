6e38ddff0ffc634d67119cb28ea7829471de2cd2
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-03-11 18:34:12 +0530

    File updated at 3/11/2026, 6:34:12 PM

--- 1423-MaximumPointsYouCanObtainfromCards/1423-MaximumPointsYouCanObtainfromCards.cpp
+++ 1423-MaximumPointsYouCanObtainfromCards/1423-MaximumPointsYouCanObtainfromCards.cpp
@@ -1,23 +1,19 @@
-// Last updated: 8/28/2025, 3:35:37 PM
-class Solution {
-public:
-    int maxScore(vector<int>& cardPoints, int k) {
-        int tot=0;
-        int n=cardPoints.size();
-        for(int n:cardPoints){
-            tot+=n;
-        }
-        int left=n-k;
-        int curr=0;
-        for(int i=0;i<left;i++){
-            curr=curr+cardPoints[i];
-        }
-        int minAns=curr;
-        for(int i=left;i<n;i++){
-            curr=curr+cardPoints[i]-cardPoints[i-left];
-            minAns=min(curr,minAns);
-        }
-        return tot-minAns;
-
-    }
-};
+// Last updated: 3/11/2026, 6:34:12 PM
+1class Solution {
+2public:
+3    int maxScore(vector<int>& cardPoints, int k) {
+4        int leftSum=0;
+5        for(int i=0;i<k;i++){
+6            leftSum+=cardPoints[i];
+7        }
+8        int maxi=leftSum;
+9        int rightSum=0, rightIndex=cardPoints.size()-1;
+10        for(int i=k-1;i>=0;i--){
+11            leftSum=leftSum-cardPoints[i];
+12            rightSum=rightSum+cardPoints[rightIndex];
+13            rightIndex--;
+14            maxi=max(maxi, leftSum+rightSum);
+15        }
+16        return maxi;
+17    }
+18};
