02c8251a812836f85c34e837510dbf21d875366e
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-03-24 20:04:34 +0530

    File updated at 3/24/2026, 8:04:33 PM

--- 652-FindDuplicateSubtrees/652-FindDuplicateSubtrees.cpp
+++ 652-FindDuplicateSubtrees/652-FindDuplicateSubtrees.cpp
@@ -1,37 +1,34 @@
-// Last updated: 9/4/2025, 4:24:04 PM
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    unordered_map<string, int> mpp;
-    string dfs(TreeNode* root, vector<TreeNode*>& res){
-        if(!root) return " ";
-        string curr=to_string(root->val);
-        string left=dfs(root->left, res);
-        string right=dfs(root->right,res);
-        string final=curr+","+left+","+right;
-        if(mpp.find(final)!=mpp.end()){
-            if(mpp[final]==1)
-            res.push_back(root);
-        }
-        mpp[final]++;
-        return final;
-
-
-    }
-    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-        if(!root) return {};
-        vector<TreeNode*> res;
-        string a=dfs(root, res);
-        return res;
-    }
-};
+// Last updated: 3/24/2026, 8:04:33 PM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14string str="";
+15unordered_map<string,int> mpp;
+16    string dfs(TreeNode* root, vector<TreeNode*>& res){
+17        if(!root) return "#";
+18        string l=dfs(root->left, res);
+19        string r=dfs(root->right,res);
+20        string curr=to_string(root->val)+","+l+","+r;
+21        if(mpp[curr]==1){
+22            res.push_back(root);
+23
+24        }
+25        mpp[curr]++;
+26        return  curr;
+27    }
+28    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+29        vector<TreeNode*> res;
+30        dfs(root, res);
+31        return res;
+32    }
+33};
