4805b8f6ed49177daee1895fa1f300cd21a170df
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-03-25 22:24:43 +0530

    File updated at 3/25/2026, 10:24:42 PM

--- 1325-DeleteLeavesWithaGivenValue/1325-DeleteLeavesWithaGivenValue.cpp
+++ 1325-DeleteLeavesWithaGivenValue/1325-DeleteLeavesWithaGivenValue.cpp
@@ -1,23 +1,23 @@
-// Last updated: 9/4/2025, 1:18:17 PM
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        if(!root) return NULL;
-        root->left=removeLeafNodes(root->left,target);
-        root->right=removeLeafNodes(root->right,target);
-        if(!root->left&&!root->right&&root->val==target)
-        root=NULL;
-        return root;
-    }
-};
+// Last updated: 3/25/2026, 10:24:42 PM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+15        if(!root) return NULL;
+16        root->left=removeLeafNodes(root->left,target);
+17        root->right=removeLeafNodes(root->right,target);
+18        if(!root->left&&!root->right&&root->val==target)
+19        root=NULL;
+20        return root;
+21    }
+22};
