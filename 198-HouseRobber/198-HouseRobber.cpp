025a2774734a7f02b49b30e46731650eca2d65be
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-03-28 23:16:03 +0530

    File updated at 3/28/2026, 11:16:02 PM

--- 198-HouseRobber/198-HouseRobber.cpp
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -1,21 +1,22 @@
-// Last updated: 3/28/2026, 11:13:56 PM
+// Last updated: 3/28/2026, 11:16:02 PM
 1class Solution {
 2public:
-3    int f(int index, vector<int>& dp, vector<int>& nums){
-4        if(index==0) return nums[0];
-5        if(index == 1) return max(nums[0], nums[1]);
-6
-7        if(dp[index]!=-1) return dp[index];
-8        //rob
-9        int r=f(index-2, dp, nums)+nums[index];
-10        //skip
-11        int s=f(index-1, dp, nums);
-12        return dp[index]= max(r,s);
-13
-14    }
-15    int rob(vector<int>& nums) {
-16        int n=nums.size();
-17        vector<int> dp(n,-1);
-18        return f(n-1, dp, nums);
-19    }
-20};
+3    int f(vector<int>& nums, int index,vector<int>& dp ){
+4        if(index==0){
+5            return nums[0];
+6        }
+7        if(index==1) return max(nums[0],nums[1]);
+8        if(dp[index]!=-1) return dp[index];
+9        //rob house
+10        int p=f(nums,index-2,dp)+nums[index];
+11        //dont rob
+12        int q=f(nums,index-1,dp);
+13        dp[index]=max(p,q);
+14        return dp[index];
+15
+16    }
+17    int rob(vector<int>& nums) {
+18        vector<int> dp(nums.size(),-1);
+19        return f(nums, nums.size()-1,dp);
+20    }
+21};
