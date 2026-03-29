8cbf397b6d56cf3d1464aaeed1eb6ad44fccbc09
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-03-30 00:29:19 +0530

    File updated at 3/30/2026, 12:29:19 AM

--- 740-DeleteandEarn/740-DeleteandEarn.cpp
+++ 740-DeleteandEarn/740-DeleteandEarn.cpp
@@ -1,21 +1,21 @@
-// Last updated: 7/31/2025, 11:58:56 PM
-class Solution {
-public:
-    int f(int i, vector<int>& count, vector<int>& dp){
-        if(i==0) return 0;
-        if(i==1) return count[1];
-        if(dp[i]!=-1) return dp[i];
-        dp[i]=max(f(i-1,count,dp), f(i-2,count,dp)+count[i]);
-        return dp[i];
-    }
-    int deleteAndEarn(vector<int>& nums) {
-        int maxNum = *max_element(nums.begin(), nums.end());
-        vector<int> count(maxNum + 1, 0);
-        for(int num:nums){
-            count[num]+=num;
-        }
-        vector<int> dp(maxNum+1,-1);
-        return f(maxNum, count, dp);
-
-    }
-};
+// Last updated: 3/30/2026, 12:29:19 AM
+1class Solution {
+2public:
+3    int f(int i,vector<int>& points, vector<int>& dp){
+4        if(i==0) return 0;
+5        if(i==1) return points[1];
+6        if(dp[i]!=-1) return dp[i];
+7        int take=f(i-2,points,dp)+points[i];
+8        int skip=f(i-1,points,dp);
+9        return dp[i]=max(take,skip);
+10    }
+11    int deleteAndEarn(vector<int>& nums) {
+12        int maxi=*max_element(nums.begin(), nums.end());
+13        vector<int> points(maxi+1,0);
+14        for(int n:nums){
+15            points[n]+=n;
+16        }
+17        vector<int> dp(maxi+1,-1);
+18        return f(maxi,points,dp);
+19    }
+20};
