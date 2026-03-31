0df4a9ccdaa4bdf20c0508423e129cf0fd9f3207
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-03-31 22:58:47 +0530

    File updated at 3/31/2026, 10:58:46 PM

--- 62-UniquePaths/62-UniquePaths.cpp
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -1,35 +1,18 @@
-// Last updated: 8/5/2025, 8:17:40 PM
-class Solution {
-public:
-    // int f(int i , int j, vector<vector<int>>& dp){
-    //     //base case:
-    //     if(i==0 && j==0)//reached top
-    //     return 1;
-    //     if(i<0 || j<0) //out of bounds of the matrix cell
-    //     return 0; 
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     int left= f(i, j-1,dp);
-    //     int right= f(i-1, j,dp);
-    //     return dp[i][j]=left+right;
-    // }
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        dp[0][0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0)
-                dp[i][j]=1;
-                else{
-                    int up=0;
-                    int left=0;
-                    if(i>0) up=dp[i-1][j];
-                    if(j>0) left=dp[i][j-1];
-                    dp[i][j]=up+left;
-
-                }
-
-            }
-        }
-        return dp[m-1][n-1];
-    }
-};
+// Last updated: 3/31/2026, 10:58:46 PM
+1class Solution {
+2public:
+3    int f(int i,int j, vector<vector<int>>& dp){
+4        if(i==0 && j==0) return 1;
+5        else if(i<0 || j<0) return 0;
+6        if(dp[i][j]!=-1) return dp[i][j];
+7        //go up
+8        int up=f(i-1,j,dp);
+9        //go left
+10        int left=f(i,j-1,dp);
+11        return dp[i][j]=up+left;
+12    }
+13    int uniquePaths(int m, int n) {
+14        vector<vector<int>> dp(m,vector<int>(n,-1));
+15        return f(m-1,n-1,dp);
+16    }
+17};
