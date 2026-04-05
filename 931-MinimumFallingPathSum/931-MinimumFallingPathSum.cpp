af0bf6480786cc6237b9ae7c6fdf0e60b3a7d5d7
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-04-05 12:45:22 +0530

    File updated at 4/5/2026, 12:45:22 PM

--- 931-MinimumFallingPathSum/931-MinimumFallingPathSum.cpp
+++ 931-MinimumFallingPathSum/931-MinimumFallingPathSum.cpp
@@ -1,41 +1,41 @@
-// Last updated: 4/28/2025, 7:20:59 PM
-class Solution {
-public:
-    // int f(int i, int j, vector<vector<int>>& matrix, int n, vector<vector<int>>& dp){
-    //     //base case:
-    //     if(i==n-1) return matrix[i][j];
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     int d= f(i+1, j, matrix, n,dp) +matrix[i][j];
-    //     int left = (j > 0) ? f(i+1, j-1, matrix, n,dp) +matrix[i][j] : INT_MAX;
-    //     int right = (j < n-1) ? f(i+1, j+1, matrix, n,dp) +matrix[i][j]: INT_MAX;
-
-    //     return dp[i][j]=min(d, min(left,right));
-
-    // }
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        
-        int n= matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n,0));
-        // int ans=INT_MAX; #memoization approach
-        // for(int j=0;j<n;j++)
-        //     ans= min(ans, f(0,j,matrix,n,dp));
-        // return ans;
-        //base case: 
-        for(int j=0;j<n;j++){
-            dp[n-1][j]=matrix[n-1][j];
-        }
-        for(int i=n-2;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                int d= dp[i+1][j]  +matrix[i][j];
-                int left = (j > 0) ? dp[i+1][j-1] +matrix[i][j] : INT_MAX;
-                int right = (j < n-1) ? dp[i+1][j+1] +matrix[i][j]: INT_MAX;
-                dp[i][j]= min(d, min(left, right));
-            }
-        }
-        int ans=INT_MAX;
-        for(int j=0;j<n;j++){
-            ans= min(ans, dp[0][j]);
-        }
-        return ans;
-    }
-};
+// Last updated: 4/5/2026, 12:45:22 PM
+1class Solution {
+2public:
+3    // int f(int i, int j, vector<vector<int>>& matrix, int n, vector<vector<int>>& dp){
+4    //     //base case:
+5    //     if(i==n-1) return matrix[i][j];
+6    //     if(dp[i][j]!=-1) return dp[i][j];
+7    //     int d= f(i+1, j, matrix, n,dp) +matrix[i][j];
+8    //     int left = (j > 0) ? f(i+1, j-1, matrix, n,dp) +matrix[i][j] : INT_MAX;
+9    //     int right = (j < n-1) ? f(i+1, j+1, matrix, n,dp) +matrix[i][j]: INT_MAX;
+10
+11    //     return dp[i][j]=min(d, min(left,right));
+12
+13    // }
+14    int minFallingPathSum(vector<vector<int>>& matrix) {
+15        
+16        int n= matrix.size();
+17        vector<vector<int>> dp(n, vector<int>(n,0));
+18        // int ans=INT_MAX; #memoization approach
+19        // for(int j=0;j<n;j++)
+20        //     ans= min(ans, f(0,j,matrix,n,dp));
+21        // return ans;
+22        //base case: 
+23        for(int j=0;j<n;j++){
+24            dp[n-1][j]=matrix[n-1][j];
+25        }
+26        for(int i=n-2;i>=0;i--){
+27            for(int j=n-1;j>=0;j--){
+28                int d= dp[i+1][j]  +matrix[i][j];
+29                int left = (j > 0) ? dp[i+1][j-1] +matrix[i][j] : INT_MAX;
+30                int right = (j < n-1) ? dp[i+1][j+1] +matrix[i][j]: INT_MAX;
+31                dp[i][j]= min(d, min(left, right));
+32            }
+33        }
+34        int ans=INT_MAX;
+35        for(int j=0;j<n;j++){
+36            ans= min(ans, dp[0][j]);
+37        }
+38        return ans;
+39    }
+40};
