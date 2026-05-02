c0ed4750aa58fb91638bc7fa0b4132bf3e6e0350
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-05-03 00:07:38 +0530

    File updated at 5/3/2026, 12:07:38 AM

--- 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.cpp
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.cpp
@@ -1,31 +1,28 @@
-// Last updated: 5/3/2026, 12:00:14 AM
+// Last updated: 5/3/2026, 12:07:38 AM
 1class Solution {
 2public:
-3    // int f(int i, int j, string &text1, string &text2, vector<vector<int>>& dp){
-4    //     if(i == 0 || j == 0) return 0;
-5    //     if(dp[i][j] != -1) return dp[i][j];
-6    //     if(text1[i-1] == text2[j-1])
-7    //         return dp[i][j] = 1 + f(i-1, j-1, text1, text2, dp);
-8    //     return dp[i][j] = max(f(i-1, j, text1, text2, dp), f(i, j-1, text1, text2, dp));
+3    // int f(string s, int l, int r,int n, vector<vector<int>>& dp){
+4    //     if(l>r) return 0;
+5    //     if(l==r) return 1;
+6    //     if(dp[l][r]!=-1) return dp[l][r];
+7    //     if(s[l]==s[r]) return dp[l][r]= 2+f(s,l+1,r-1,n,dp);
+8    //     return dp[l][r]=max(f(s,l+1,r,n,dp), f(s,l,r-1,n,dp));
 9    // }
 10    int longestPalindromeSubseq(string s) {
-11        string s2= s;
-12        reverse(s2.begin(), s2.end());
-13        int n= s.size();
-14        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-15        for (int j = 0; j <= n; j++)
-16            dp[0][j] = 0;
-17        for (int i = 0; i <= n; i++)
-18            dp[i][0] = 0;
-19        for (int i = 1; i <= n; i++) {
-20            for (int j = 1; j <= n; j++) {
-21                if (s[i - 1] == s2[j - 1])
-22                    dp[i][j] = 1 + dp[i - 1][j - 1];
-23                else
-24                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-25            }
-26        }
-27        return dp[n][n];
-28        //return f(n, n, s, s2, dp);
-29    }
-30};
+11        int len=s.size();
+12        int l=0, r=len-1;
+13        vector<vector<int>> dp(len+1, vector<int>(len+1,0));
+14        //return f(s, l,r,len,dp);
+15        for(int i=0;i<len;i++) dp[i][i]=1;
+16        for(int l=len-1;l>=0;l--){
+17            for(int r=l+1;r<len;r++){
+18                if (s[l] == s[r]) {
+19                    dp[l][r] = 2 + dp[l + 1][r - 1];
+20                } else {
+21                    dp[l][r] = max(dp[l + 1][r], dp[l][r - 1]);
+22                }
+23            }
+24        }
+25        return dp[0][len-1];
+26    }
+27};
