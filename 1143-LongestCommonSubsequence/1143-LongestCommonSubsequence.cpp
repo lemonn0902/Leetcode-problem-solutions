9e4b47dc176debd81b90fab9aa5ee36ec2c0d407
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-05-02 23:15:19 +0530

    File updated at 5/2/2026, 11:15:19 PM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
@@ -1,32 +1,33 @@
-// Last updated: 5/18/2025, 12:53:07 PM
-class Solution {
-public:
-    // memoization:
-    //  int f(int i, int j, string &text1, string &text2, vector<vector<int>>&
-    //  dp){
-    //      if(i == 0 || j == 0) return 0;
-    //      if(dp[i][j] != -1) return dp[i][j];
-    //      if(text1[i-1] == text2[j-1])
-    //          return dp[i][j] = 1 + f(i-1, j-1, text1, text2, dp);
-    //      return dp[i][j] = max(f(i-1, j, text1, text2, dp), f(i, j-1, text1,
-    //      text2, dp));
-    //  }
-
-    int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size(), m = text2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        for (int j = 0; j <= m; j++)
-            dp[0][j] = 0;
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (text1[i - 1] == text2[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-        return dp[n][m];
-    }
-};
+// Last updated: 5/2/2026, 11:15:19 PM
+1class Solution {
+2public:
+3    // memoization:
+4    //  int f(int i, int j, string &text1, string &text2, vector<vector<int>>&
+5    //  dp){
+6    //      if(i == 0 || j == 0) return 0;
+7    //      if(dp[i][j] != -1) return dp[i][j];
+8    //      if(text1[i-1] == text2[j-1])
+9    //          return dp[i][j] = 1 + f(i-1, j-1, text1, text2, dp);
+10    //      return dp[i][j] = max(f(i-1, j, text1, text2, dp), f(i, j-1, text1,
+11    //      text2, dp));
+12    //  }
+13
+14    int longestCommonSubsequence(string text1, string text2) {
+15        int n = text1.size(), m = text2.size();
+16        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+17        for (int j = 0; j <= m; j++)
+18            dp[0][j] = 0;
+19        for (int i = 0; i <= n; i++)
+20            dp[i][0] = 0;
+21        for (int i = 1; i <= n; i++) {
+22            for (int j = 1; j <= m; j++) {
+23                if (text1[i - 1] == text2[j - 1])
+24                    dp[i][j] = 1 + dp[i - 1][j - 1];
+25                else
+26                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+27            }
+28        }
+29        return dp[n][m];
+30    }
+31};
+32
