ba895ce7ed4349a4d78bade2e23fc20b55e9c850
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-05-03 22:40:26 +0530

    File updated at 5/3/2026, 10:40:26 PM

--- 1312-MinimumInsertionStepstoMakeaStringPalindrome/1312-MinimumInsertionStepstoMakeaStringPalindrome.cpp
+++ 1312-MinimumInsertionStepstoMakeaStringPalindrome/1312-MinimumInsertionStepstoMakeaStringPalindrome.cpp
@@ -1,32 +1,17 @@
-// Last updated: 5/22/2025, 10:33:34 PM
-class Solution {
-public:
-//memoization: 
-    // int f(int i, int j, string& s, vector<vector<int>>& dp){
-    //     if(i>=j){
-    //         return 0;
-    //     }
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     if(s[i]==s[j]){
-    //         return 0+f(i+1,j-1,s,dp);
-    //     }
-    //     else
-    //     return 1+min(f(i+1, j, s,dp), f(i,j-1,s,dp));
-    // }
-    int minInsertions(string s) {
-        int n=s.size();
-        vector<vector<int>> dp(n, vector<int>(n,0));
-        for(int len=2;len<=n;len++){
-            for(int i=0;i<=n-len;i++){
-                int j=i+len-1;
-                if (s[i] == s[j])
-                    dp[i][j] = dp[i + 1][j - 1];
-                else
-                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1]);
-            }
-        }
-        // return f(0,n-1,s,dp);
-        return dp[0][n-1];
-    }
-    
-};
+// Last updated: 5/3/2026, 10:40:26 PM
+1class Solution {
+2public:
+3    int minInsertions(string s) {
+4        int n=s.size();
+5        vector<vector<int>> dp(n, vector<int>(n,0));
+6        for(int l=n-1;l>=0;l--){
+7            for(int r=l+1;r<n;r++){
+8                if(s[l]==s[r]) dp[l][r]=dp[l+1][r-1];
+9                else{
+10                    dp[l][r]=min( dp[l+1][r], dp[l][r-1])+1;
+11                }
+12            }
+13        }
+14        return dp[0][n-1];
+15    }
+16};
