7caf18f9f785d69e1dd1e8a33b3ba9c239f82b8a
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-05-04 22:00:27 +0530

    File updated at 5/4/2026, 10:00:27 PM

--- 583-DeleteOperationforTwoStrings/583-DeleteOperationforTwoStrings.cpp
+++ 583-DeleteOperationforTwoStrings/583-DeleteOperationforTwoStrings.cpp
@@ -1,20 +1,26 @@
-// Last updated: 5/22/2025, 10:57:34 PM
-class Solution {
-public:
-    int minDistance(string word1, string word2) {
-        int m=word1.size();
-        int n=word2.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
-        return m-dp[m][n] + n-dp[m][n];
-    }
-};
+// Last updated: 5/4/2026, 10:00:27 PM
+1class Solution {
+2public:
+3    int minDistance(string word1, string word2) {
+4        int n = word1.size();
+5        int m = word2.size();
+6        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+7        for (int j = 0; j <= m; j++) {
+8            dp[n][j] = m - j;
+9        }
+10        for (int i = 0; i <= n; i++) {
+11            dp[i][m] = n - i;
+12        }
+13        for (int i = n - 1; i >= 0; i--) {
+14            for (int j = m - 1; j >= 0; j--) {
+15
+16                if (word1[i] == word2[j]) {
+17                    dp[i][j] = dp[i+1][j+1];
+18                } else {
+19                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j+1]);
+20                }
+21            }
+22        }
+23        return dp[0][0];
+24    }
+25};
