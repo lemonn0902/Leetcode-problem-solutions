a3c96a45ed1ee8899a614e7cf8fd8c7178f9abd5
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-05-07 23:35:41 +0530

    File updated at 5/7/2026, 11:35:41 PM

--- 139-WordBreak/139-WordBreak.cpp
+++ 139-WordBreak/139-WordBreak.cpp
@@ -1,25 +1,20 @@
-// Last updated: 6/29/2025, 1:08:05 AM
-class Solution {
-public:
-    unordered_map<int, bool> memo;
-    bool backtrack(string s, vector<string>& wordDict, int start){
-        
-        if(start==s.size())
-        {
-            return true;
-        }
-        if(memo.count(start)) return memo[start];
-        for(int i=0;i<wordDict.size();i++){
-            string str= wordDict[i]; 
-            if(s.substr(start, str.length())==str){
-                if(backtrack(s, wordDict, start+str.length()))
-                    return memo[start]=true;
-            }
-
-        }
-        return memo[start]=false;
-    }
-    bool wordBreak(string s, vector<string>& wordDict) {
-        return backtrack(s,wordDict,0);
-    }
-};
+// Last updated: 5/7/2026, 11:35:41 PM
+1class Solution {
+2public:
+3    bool f(string s, vector<string>& wordDict,vector<int>& dp,int idx){
+4        if(idx==s.size()) return true;
+5        if(dp[idx]!=-1) return dp[idx];
+6        for(string& str:wordDict){
+7            int len=str.size();
+8            if(idx+len<=s.size()&&s.substr(idx,len)==str){
+9                if(f(s,wordDict,dp,idx+len)) return dp[idx]=true;
+10            }
+11        }
+12        return dp[idx]=false;
+13
+14    }
+15    bool wordBreak(string s, vector<string>& wordDict) {
+16        vector<int> dp(s.size(), -1);
+17        return f(s,wordDict,dp,0);
+18    }
+19};
