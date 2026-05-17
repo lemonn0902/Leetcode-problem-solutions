e71a29284ffa7173a43f869988f6bd6ffabfffd1
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-05-17 13:04:21 +0530

    File updated at 5/17/2026, 1:04:21 PM

--- 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.cpp
+++ 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.cpp
@@ -1,22 +1,21 @@
-// Last updated: 7/2/2025, 12:39:42 AM
-class Solution {
-public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<vector<int>> res;
-        priority_queue<pair<int, vector<int>>> pq;
-        for(auto& it: points){
-            int x=it[0];
-            int y=it[1];
-            int d= x*x + y*y;
-            pq.push({d, it});
-            if(pq.size()>k) pq.pop();
-        }
-        
-        while (!pq.empty()) {
-            res.push_back(pq.top().second);
-            pq.pop();
-        }
-
-        return res;
-    }
-};
+// Last updated: 5/17/2026, 1:04:21 PM
+1class Solution {
+2public:
+3    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+4        int n=points.size();
+5        priority_queue<pair<int,vector<int>>, vector<pair<int,vector<int>>>, greater<pair<int, vector<int>>>> pq;
+6        vector<vector<int>> ans;
+7        for(int i=0;i<n;i++){
+8            int x=points[i][0];
+9            int y=points[i][1];
+10            int d= x*x + y*y;
+11            pq.push({d,points[i]});
+12        }
+13        while(k){
+14            ans.push_back(pq.top().second);
+15            pq.pop();
+16            k--;
+17        }
+18        return ans;
+19    }
+20};
