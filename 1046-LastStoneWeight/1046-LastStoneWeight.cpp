4d161ee0d4f3d75ab0ea4ba592b0cd3d803baf02
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-05-17 12:04:22 +0530

    File updated at 5/17/2026, 12:04:21 PM

--- 1046-LastStoneWeight/1046-LastStoneWeight.cpp
+++ 1046-LastStoneWeight/1046-LastStoneWeight.cpp
@@ -1,22 +1,18 @@
-// Last updated: 7/2/2025, 12:18:00 AM
-class Solution {
-public:
-    int lastStoneWeight(vector<int>& stones) {
-        if(stones.size()==1) return stones[0];
-        priority_queue<int> pq(stones.begin(), stones.end());
-        
-        
-        while(pq.size()>1){
-            int y= pq.top();
-            pq.pop();
-            int x= pq.top();
-            pq.pop();
-            
-            if(x!=y){
-                
-                pq.push(y-x);
-            }
-        }
-        return pq.size()==1? pq.top():0;
-    }
-};
+// Last updated: 5/17/2026, 12:04:21 PM
+1class Solution {
+2public:
+3    int lastStoneWeight(vector<int>& stones) {
+4        priority_queue<int> pq(stones.begin(), stones.end());
+5        while(pq.size()>1){
+6            int x= pq.top();
+7            pq.pop();
+8            int y=pq.top();
+9            pq.pop();
+10            if(x==y) continue;
+11            else{
+12                pq.push( abs(x-y) );
+13            }
+14        }
+15        return pq.size()==0? 0:pq.top();
+16    }
+17};
