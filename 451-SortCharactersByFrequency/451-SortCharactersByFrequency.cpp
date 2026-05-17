9bcf2d4105c6e7a6bdfb14b067bbf0211470c472
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-05-17 15:36:00 +0530

    File updated at 5/17/2026, 3:35:59 PM

--- 451-SortCharactersByFrequency/451-SortCharactersByFrequency.cpp
+++ 451-SortCharactersByFrequency/451-SortCharactersByFrequency.cpp
@@ -1,20 +1,20 @@
-// Last updated: 8/30/2025, 4:13:32 PM
-class Solution {
-public:
-    string frequencySort(string s) {
-        unordered_map<char,int> freq;
-        for(char ch:s){
-            freq[ch]++;
-        }
-        vector<pair<char,int>> freqVec(freq.begin(), freq.end());
-        sort(freqVec.begin(), freqVec.end(), [](const pair<char,int>& a,const pair<char, int>& b){
-            return a.second>b.second;
-        });
-        string res;
-        for(auto it:freqVec){
-            res.append(it.second,it.first);
-        }
-        return res;
-
-    }
-};
+// Last updated: 5/17/2026, 3:35:59 PM
+1class Solution {
+2public:
+3    string frequencySort(string s) {
+4        priority_queue<pair<int,char>> pq;
+5        unordered_map<char,int> freq;
+6        for(char ch:s){
+7            freq[ch]++;
+8        }
+9        for(auto it:freq) pq.push({it.second, it.first});
+10        string ans;
+11        while(pq.size()){
+12            char c=pq.top().second;
+13            
+14            for(int i=0;i<pq.top().first;i++) ans+=c;
+15            pq.pop();
+16        }
+17        return ans;
+18    }
+19};
