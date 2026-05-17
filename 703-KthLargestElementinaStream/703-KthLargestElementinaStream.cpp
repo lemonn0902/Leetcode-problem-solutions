b12b75a2a4d58585529636e7c2a786fca178f098
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-05-17 11:50:31 +0530

    File updated at 5/17/2026, 11:50:31 AM

--- 703-KthLargestElementinaStream/703-KthLargestElementinaStream.cpp
+++ 703-KthLargestElementinaStream/703-KthLargestElementinaStream.cpp
@@ -1,27 +1,23 @@
-// Last updated: 7/1/2025, 9:25:21 PM
-class KthLargest {
-    int num;
-    priority_queue<int, vector<int>, greater<int>> minHeap;
-public:
-
-    KthLargest(int k, vector<int>& nums) {
-        num=k;
-        for(auto ele : nums){
-            minHeap.push(ele);
-            if(minHeap.size()>k) minHeap.pop();
-        }
-    }
-    
-    int add(int val) {
-        minHeap.push(val);
-        if(minHeap.size()>num) minHeap.pop();
-        return minHeap.top();
-
-    }
-};
-
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest* obj = new KthLargest(k, nums);
- * int param_1 = obj->add(val);
- */
+// Last updated: 5/17/2026, 11:50:31 AM
+1class KthLargest {
+2public:
+3priority_queue<int, vector<int>, greater<int>> pq;
+4int num;
+5    KthLargest(int k, vector<int>& nums) {
+6        num=k;
+7        for(int n:nums) pq.push(n);
+8
+9    }
+10    
+11    int add(int val) {
+12        pq.push(val);
+13        while(pq.size()>num) pq.pop();
+14        return pq.top();
+15    }
+16};
+17
+18/**
+19 * Your KthLargest object will be instantiated and called as such:
+20 * KthLargest* obj = new KthLargest(k, nums);
+21 * int param_1 = obj->add(val);
+22 */
