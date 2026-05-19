5fa144055efd489dcd8f00017f1cea5c80950d9c
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-05-19 23:19:10 +0530

    File updated at 5/19/2026, 11:19:10 PM

--- 846-HandofStraights/846-HandofStraights.cpp
+++ 846-HandofStraights/846-HandofStraights.cpp
@@ -1,17 +1,20 @@
-// Last updated: 7/8/2025, 1:53:37 AM
-class Solution {
-public:
-    bool isNStraightHand(vector<int>& hand, int groupSize) {
-        if (hand.size() % groupSize != 0) return false;
-        map<int, int> freq;
-        for (int card : hand) freq[card]++;
-        for (auto [num, count] : freq) {
-            if(count == 0) continue;
-            for (int i = 0; i < groupSize; ++i) {
-                if (freq[num + i] < count) return false;
-                freq[num + i] -= count;
-            }
-        }
-        return true;
-    }
-};
+// Last updated: 5/19/2026, 11:19:10 PM
+1class Solution {
+2public:
+3    bool isNStraightHand(vector<int>& hand, int groupSize) {
+4        int n=hand.size();
+5        if(n%groupSize!=0) return 0;
+6        map<int,int> freq;
+7        for(int n:hand) freq[n]++;
+8        while(!freq.empty()){
+9            int start=freq.begin()->first;
+10            for(int i=0;i<groupSize;i++){
+11                int card=start+i;
+12                if(freq[card]==0) return false;
+13                freq[card]--;
+14                if(freq[card]==0) freq.erase(card);
+15            }
+16        }
+17        return true;
+18    }
+19};
