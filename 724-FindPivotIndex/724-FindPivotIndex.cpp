12e72c4571556aa407fddb39d1cc33991f35acea
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-07-08 22:11:28 +0530

    File updated at 7/8/2026, 10:11:27 PM

--- 724-FindPivotIndex/724-FindPivotIndex.cpp
+++ 724-FindPivotIndex/724-FindPivotIndex.cpp
@@ -1,19 +1,18 @@
-// Last updated: 7/31/2025, 8:49:47 PM
-class Solution {
-public:
-    int pivotIndex(vector<int>& nums) {
-        int n=nums.size();
-        int sum=0;
-        int SumRight=0;
-        int SumLeft=0;
-        for(int i=0;i<n;i++){
-            sum=sum+nums[i];
-        }
-        for(int i=0;i<n;i++){
-            SumRight=sum-nums[i]-SumLeft;
-            if(SumLeft==SumRight) return i;
-            SumLeft+=nums[i];
-        }
-        return -1;
-    }
-};
+// Last updated: 7/8/2026, 10:11:27 PM
+1class Solution {
+2public:
+3    int pivotIndex(vector<int>& nums) {
+4        int tot=0;
+5        int n=nums.size();
+6        vector<int> prefix(n+1,0);
+7        for(int i=0;i<n;i++){
+8            prefix[i+1]=prefix[i]+nums[i];
+9            tot+=nums[i];
+10        }
+11        for(int i=0;i<n;i++){
+12            int rem=tot-prefix[i]-nums[i];
+13            if(rem==prefix[i]) return i;
+14        }
+15        return -1;
+16    }
+17};
