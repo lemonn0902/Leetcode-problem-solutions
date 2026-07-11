5f4a40be9261ab8cea663b1d733b7744a72a74f8
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-07-11 23:01:14 +0530

    File updated at 7/11/2026, 11:01:14 PM

--- 525-ContiguousArray/525-ContiguousArray.cpp
+++ 525-ContiguousArray/525-ContiguousArray.cpp
@@ -1,23 +1,20 @@
-// Last updated: 9/13/2025, 5:38:03 PM
-class Solution {
-public:
-    int findMaxLength(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int,int> mpp;
-        mpp[0]=-1;
-        int ans=0;
-        int prefixSum=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==1){
-                prefixSum+=1;
-            }
-            else prefixSum-=1;
-            if(mpp.count(prefixSum)){
-                ans=max(ans,i-mpp[prefixSum]);
-            }else{
-                mpp[prefixSum]=i;
-            }
-        }
-        return ans;
-    }
-};
+// Last updated: 7/11/2026, 11:01:14 PM
+1class Solution {
+2public:
+3    int findMaxLength(vector<int>& nums) {
+4        unordered_map<int,int> mpp; //prefixSum, first occurence index
+5        int prefix=0;
+6        mpp[0]=-1;
+7        int n=nums.size();
+8        int maxi=0;
+9        for(int i=0;i<n;i++){
+10            if(nums[i]==1) prefix++;
+11            else prefix--;
+12            if(mpp.find(prefix)!=mpp.end()){
+13                maxi=max(maxi, i-mpp[prefix]);
+14            }
+15            else mpp[prefix]=i;
+16        }
+17        return maxi;
+18    }
+19};
