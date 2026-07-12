0ae43d776e2c236a623d5b3639e01c2f4ba553b0
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-07-12 12:20:58 +0530

    File updated at 7/12/2026, 12:20:58 PM

--- 20-ValidParentheses/20-ValidParentheses.cpp
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -1,27 +1,5 @@
-// Last updated: 7/29/2025, 5:08:10 PM
-class Solution {
-public:
-    bool isValid(string s) {
-        if(s[0]==']'||s[0]=='}'||s[0]==')') return false;
-        if(s.size()==1) return false;
-        stack<char> st;
-        unordered_map<char,char> mpp={
-            {')','('},
-            {'}','{'},
-            {']','['}
-        };
-        for(char c: s){
-            if(mpp.find(c)==mpp.end()){
-                //it is an opening bracket
-                st.push(c);
-            }
-            else if(!st.empty()&& mpp[c]==st.top()){
-                st.pop();
-            }
-            else{
-                return false;
-            }
-        }
-        return st.empty();
-    }
-};
+// Last updated: 7/12/2026, 12:20:58 PM
+else if(ch=='}'){
+    if(last!='{') return false;
+}
+else st.pop();  // ❌ Only executes when ch != '}'
