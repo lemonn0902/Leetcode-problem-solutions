e9947c42ca7d55abeacc933d58d2999bd599c002
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-07-17 00:19:03 +0530

    File updated at 7/17/2026, 12:19:03 AM

--- 1475-FinalPricesWithaSpecialDiscountinaShop/1475-FinalPricesWithaSpecialDiscountinaShop.cpp
+++ 1475-FinalPricesWithaSpecialDiscountinaShop/1475-FinalPricesWithaSpecialDiscountinaShop.cpp
@@ -1,26 +1,20 @@
-// Last updated: 8/28/2025, 12:41:21 PM
-class Solution {
-public:
-    vector<int> finalPrices(vector<int>& prices) {
-        int n=prices.size();
-        vector<int> ans(n);
-        stack<int> st;
-        
-        for(int i=n-1;i>=0;i--){
-            
-            while(!st.empty()&&st.top()>prices[i]){
-                
-                st.pop();
-            }
-            if(!st.empty()) {
-                ans[i]=prices[i]-st.top();
-            } 
-            else {
-                ans[i]=prices[i];
-            }
-            st.push(prices[i]);
-        }
-        
-        return ans;
-    }
-};
+// Last updated: 7/17/2026, 12:19:03 AM
+1class Solution {
+2public:
+3    vector<int> finalPrices(vector<int>& prices) {
+4        int n=prices.size();
+5        vector<int>res(n);
+6        stack<int> st;
+7        for(int i=n-1;i>=0;i--){
+8            while(!st.empty() && st.top()>prices[i]){
+9                st.pop();
+10            }
+11            res[i] = (st.empty())?0: st.top();
+12            st.push(prices[i]);
+13        }
+14        for(int i=0;i<n;i++){
+15            prices[i]=prices[i]-res[i];
+16        }
+17        return prices;
+18    }
+19};
