365530b9abd23f6d2834f01fb212e45b87baff79
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-07-30 00:56:43 +0530

    File updated at 7/30/2026, 12:56:42 AM

--- 503-NextGreaterElementII/503-NextGreaterElementII.cpp
+++ 503-NextGreaterElementII/503-NextGreaterElementII.cpp
@@ -1,29 +1,20 @@
-// Last updated: 7/30/2025, 12:44:34 AM
-class Solution {
-public:
-    vector<int> nextGreaterElements(vector<int>& nums) {
-        int n= nums.size();
-        stack<int> st;
-        vector<int> res;
-        
-        for(int i=2*n-1;i>=0;i--){
-            
-                while(!st.empty()&& st.top()<=nums[i%n]){
-                st.pop();
-            }
-            if(i<n){
-                if(st.empty())
-            res.push_back(-1);
-            else 
-            res.push_back(st.top());
-            }
-            
-            st.push(nums[i%n]);
-            
-            
-        }
-        reverse(res.begin(), res.end());
-        return res;
-        
-    }
-};
+// Last updated: 7/30/2026, 12:56:42 AM
+1class Solution {
+2public:
+3    vector<int> nextGreaterElements(vector<int>& nums) {
+4        int n = nums.size();
+5        vector<int> res(n, -1);
+6        stack<int> st;
+7        for (int i = 2 * n - 1; i >= 0; i--) {
+8            while (!st.empty() && st.top() <= nums[i % n]) {
+9                st.pop();
+10            }
+11            if (i < n) {
+12                if (!st.empty())
+13                    res[i] = st.top();
+14            }
+15            st.push(nums[i % n]);
+16        }
+17        return res;
+18    }
+19};
