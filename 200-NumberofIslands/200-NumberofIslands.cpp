8909a08f4bdbf6011bae37d4ab578c88f08c962f
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-04 15:51:13 +0530

    File updated at 8/4/2026, 3:51:13 PM

--- 200-NumberofIslands/200-NumberofIslands.cpp
+++ 200-NumberofIslands/200-NumberofIslands.cpp
@@ -1,34 +1,29 @@
-// Last updated: 8/9/2025, 11:52:03 AM
-class Solution {
-public:
-    void dfs(vector<vector<char>>& grid, int i,int j){
-        int m=grid.size();
-        int n=grid[0].size();
-        if(i<0||i>=m||j<0||j>=n){
-            return;
-        }
-        if(grid[i][j]=='0') return;
-        grid[i][j]='0';
-        dfs(grid, i+1,j);
-        dfs(grid, i-1,j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
-    }
-    int countIslands(vector<vector<char>>& grid){
-        int tot=0;
-        int rows=grid.size();
-        int cols=grid[0].size();
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]=='1'){
-                    tot+=1;
-                    dfs(grid, i, j);
-                }
-            }
-        }
-        return tot;
-    }
-    int numIslands(vector<vector<char>>& grid) {
-        return countIslands(grid);
-    }
-};
+// Last updated: 8/4/2026, 3:51:13 PM
+1class Solution {
+2public:
+3    void dfs(int i,int j,vector<vector<char>>& grid){
+4        int m=grid.size();
+5        int n=grid[0].size();
+6        if(i>=m||j>=n||i<0||j<0) return;
+7        if(grid[i][j]=='0') return;
+8        grid[i][j]='0';
+9        dfs(i+1,j,grid);
+10        dfs(i,j+1,grid);
+11        dfs(i-1,j,grid);
+12        dfs(i,j-1,grid);
+13    }
+14    int numIslands(vector<vector<char>>& grid) {
+15        int tot=0;
+16        int rows=grid.size();
+17        int cols=grid[0].size();
+18        for(int i=0;i<rows;i++){
+19            for(int j=0;j<cols;j++){
+20                if(grid[i][j]=='1'){
+21                    tot+=1;
+22                    dfs(i,j,grid);
+23                }
+24            }
+25        }
+26        return tot;
+27    }
+28};
