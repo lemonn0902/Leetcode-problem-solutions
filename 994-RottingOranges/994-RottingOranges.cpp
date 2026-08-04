5fff94f5fc1c5948846a9063b58719dda447ee19
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-04 19:46:05 +0530

    File updated at 8/4/2026, 7:46:05 PM

--- 994-RottingOranges/994-RottingOranges.cpp
+++ 994-RottingOranges/994-RottingOranges.cpp
@@ -1,48 +1,25 @@
-// Last updated: 8/4/2026, 7:06:22 PM
+// Last updated: 8/4/2026, 7:46:05 PM
 1class Solution {
 2public:
-3    int orangesRotting(vector<vector<int>>& grid) {
-4        int n = grid.size();
-5        int m = grid[0].size();
-6        queue<pair<int, int>> q;
-7        int fresh = 0;
-8        for (int i = 0; i < n; i++) {
-9            for (int j = 0; j < m; j++) {
-10                if (grid[i][j] == 2)
-11                    q.push({i, j});
-12
-13                else if (grid[i][j] == 1)
-14                    fresh++;
-15            }
-16        }
-17        if (fresh == 0)
-18            return 0;
-19        vector<int> dr = {-1, 1, 0, 0};
-20        vector<int> dc = {0, 0, -1, 1};
-21        int tot = 0;
-22        while (!q.empty()) {
-23            int sz = q.size();
-24            bool isLevel=false;
-25            while (sz--) {
-26                auto [r, c] = q.front();
-27                q.pop();
-28                for (int k = 0; k < 4; k++) {
-29                    int nr = r + dr[k];
-30                    int nc = c + dc[k];
-31                    if (nr < 0 || nc < 0 || nr >= n || nc >= m)
-32                        continue;
-33                    if (grid[nr][nc] == 1) {
-34                        q.push({nr, nc});
-35                        grid[nr][nc] = 2;
-36                        fresh--;
-37                        isLevel=true;
-38                    }
-39                }
-40            }
-41            if (isLevel)
-42                tot++;
-43        }
-44        if(fresh!=0) return -1;
-45        return tot;
-46    }
-47};
+3    void dfs(int node, vector<vector<int>>& isConnected,vector<int>& vis){
+4        vis[node]=1;
+5        for(int i=0;i<isConnected.size();i++){
+6            if(isConnected[node][i]==1&&!vis[i]){
+7                dfs(i,isConnected,vis);
+8            }
+9        }
+10    }
+11    int findCircleNum(vector<vector<int>>& isConnected) {
+12        int n=isConnected.size();
+13        vector<int> vis(n,0);
+14        int ans=0;
+15        for(int i=0;i<n;i++){
+16            if(vis[i]==0){
+17                dfs(i, isConnected,vis);
+18                ans++;
+19            }
+20        }
+21        return ans;
+22        
+23    }
+24};
