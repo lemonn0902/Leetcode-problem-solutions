536276681b3b6e7cffdcce0428193acef9aeb171
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-05 17:16:34 +0530

    File updated at 8/5/2026, 5:16:33 PM

--- 210-CourseScheduleII/210-CourseScheduleII.cpp
+++ 210-CourseScheduleII/210-CourseScheduleII.cpp
@@ -1,34 +1,45 @@
-// Last updated: 8/5/2026, 4:37:29 PM
+// Last updated: 8/5/2026, 5:16:33 PM
 1class Solution {
 2public:
-3    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-4        vector<vector<int>> adj(numCourses);
-5        for(auto& e:prerequisites){
-6            adj[e[1]].push_back(e[0]);
-7        }
-8        vector<int> indegree(numCourses,0);
-9        for(int i=0;i<numCourses;i++){
-10            for(int it:adj[i]){
-11                indegree[it]++;
+3    void transform(string& curr, unordered_set<string>& transformed) {
+4        int len = curr.size();
+5        for (int i = 0; i < len; i++) {
+6            char original = curr[i];
+7            for (char ch = 'a'; ch <= 'z'; ch++) {
+8                if (ch == original)
+9                    continue;
+10                curr[i] = ch;
+11                transformed.insert(curr);
 12            }
-13        }
-14        queue<int> q;
-15        for(int i=0;i<indegree.size();i++){
-16            if(indegree[i]==0) q.push(i);
-17        }
-18        vector<int> topo;
-19        int c=0;
-20        while(!q.empty()){
-21            int node=q.front();
-22            q.pop();
-23            c++;
-24            topo.push_back(node);
-25            for(int next:adj[node]){
-26                indegree[next]--;
-27                if(indegree[next]==0) q.push(next);
-28            }
-29        }
-30        if(c!=numCourses) return {};
-31        return topo;
-32    }
-33};
+13            curr[i] = original;
+14        }
+15    }
+16    int ladderLength(string beginWord, string endWord,
+17                     vector<string>& wordList) {
+18        queue<string> q;
+19        q.push(beginWord);
+20        int level = 1;
+21        unordered_set<string> st(wordList.begin(), wordList.end());
+22        if(st.find(endWord)==st.end()) return 0;
+23        while (!q.empty()) {
+24            int sz = q.size();
+25            while (sz--) {
+26                string curr = q.front();
+27                if (curr == endWord)
+28                    return level;
+29                q.pop();
+30                
+31                unordered_set<string> transformed;
+32                transform(curr, transformed);
+33                for (string s : transformed) {
+34                    if (st.find(s) != st.end()) {
+35                        q.push(s);
+36                        st.erase(s);
+37                    }
+38                }
+39            }
+40            level++;
+41        }
+42        return 0;
+43    }
+44};
