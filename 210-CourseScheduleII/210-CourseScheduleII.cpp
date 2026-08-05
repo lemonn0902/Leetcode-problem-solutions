21597031bd6e56c65a83cf87903dcc52b98997e3
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-05 16:37:29 +0530

    File updated at 8/5/2026, 4:37:29 PM

--- 210-CourseScheduleII/210-CourseScheduleII.cpp
+++ 210-CourseScheduleII/210-CourseScheduleII.cpp
@@ -1,33 +1,34 @@
-// Last updated: 8/5/2026, 4:36:10 PM
+// Last updated: 8/5/2026, 4:37:29 PM
 1class Solution {
 2public:
 3    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
 4        vector<vector<int>> adj(numCourses);
-5        int m= prerequisites.size();
-6        for(int i=0;i<m;i++){
-7            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-8        }
-9        queue<int> q;
-10        vector<int> topo;
-11        vector<int> indeg(numCourses, 0);
-12        for(int i=0;i<numCourses;i++){
-13            for(auto it: adj[i]) indeg[it]++;
-14        }
-15        for(int i=0;i<numCourses;i++){
-16            if(indeg[i]==0) q.push(i);
+5        for(auto& e:prerequisites){
+6            adj[e[1]].push_back(e[0]);
+7        }
+8        vector<int> indegree(numCourses,0);
+9        for(int i=0;i<numCourses;i++){
+10            for(int it:adj[i]){
+11                indegree[it]++;
+12            }
+13        }
+14        queue<int> q;
+15        for(int i=0;i<indegree.size();i++){
+16            if(indegree[i]==0) q.push(i);
 17        }
-18        int count=0;
-19        while(!q.empty()){
-20            int node= q.front();
-21            q.pop();
-22            count++;
-23            topo.push_back(node);
-24            for(auto it: adj[node]){
-25                indeg[it]--;
-26                if(indeg[it]==0) q.push(it);
-27            }
-28        }
-29        if(count!=numCourses) return {};
-30        return topo;
-31    }
-32};
+18        vector<int> topo;
+19        int c=0;
+20        while(!q.empty()){
+21            int node=q.front();
+22            q.pop();
+23            c++;
+24            topo.push_back(node);
+25            for(int next:adj[node]){
+26                indegree[next]--;
+27                if(indegree[next]==0) q.push(next);
+28            }
+29        }
+30        if(c!=numCourses) return {};
+31        return topo;
+32    }
+33};
