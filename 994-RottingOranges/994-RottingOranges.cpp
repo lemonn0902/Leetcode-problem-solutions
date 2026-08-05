09e8e813b0f7cafdc1c4b350a21773ebc71924e8
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-05 14:36:03 +0530

    File updated at 8/5/2026, 2:36:03 PM

--- 994-RottingOranges/994-RottingOranges.cpp
+++ 994-RottingOranges/994-RottingOranges.cpp
@@ -1,32 +1,30 @@
-// Last updated: 8/5/2026, 2:05:59 PM
+// Last updated: 8/5/2026, 2:36:03 PM
 1class Solution {
 2public:
-3    struct DSU{
-4        vector<int> parent, rank_;
-5        DSU(int n): parent(n), rank_(n,0){
-6            iota(parent.begin(), parent.end(),0);
+3    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+4        vector<vector<int>> adj(numCourses);
+5        for(auto e:prerequisites){
+6            adj[e[1]].push_back(e[0]);
 7        }
-8        int find(int x){
-9            if(parent[x]!=x) parent[x]=find(parent[x]);
-10            return parent[x];
+8        vector<int> indegree(numCourses,0);
+9        for(int i=0;i<numCourses;i++){
+10            for(auto it:adj[i]) indegree[it]++;
 11        }
-12        bool unite(int a, int b){
-13            int ra=find(a), rb=find(b);
-14            if(ra==rb) return false;
-15            if(rank_[ra]<rank_[rb]) swap(ra,rb);
-16            parent[rb]=ra;
-17            if(rank_[ra]==rank_[rb]) rank_[ra]++;
-18            return true;
-19        }
-20    };
-21    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-22        int n=edges.size();
-23        DSU dsu(n+1);
-24        for(auto& e:edges){
-25            if(!dsu.unite(e[0], e[1])){
-26                return e;
-27            }
-28        }
-29        return {};
-30    }
-31};
+12        queue<int> q;
+13        for(int i=0;i<indegree.size();i++){
+14            if(indegree[i]==0) q.push(i);
+15        }
+16        int len=0;
+17        while(!q.empty()){
+18            int node=q.front();
+19            q.pop();
+20            len++;
+21            for(int next:adj[node]){
+22                indegree[next]--;
+23                if(indegree[next]==0) q.push(next);
+24            }
+25        }
+26        if(len==numCourses) return true;
+27        else return false;
+28    }
+29};
