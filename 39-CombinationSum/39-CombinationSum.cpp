0705e74386993ac8929f38e044bdff233f480132
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-07 00:51:33 +0530

    File updated at 8/7/2026, 12:51:33 AM

--- 39-CombinationSum/39-CombinationSum.cpp
+++ 39-CombinationSum/39-CombinationSum.cpp
@@ -1,26 +1,30 @@
-// Last updated: 10/26/2025, 3:18:06 PM
-class Solution {
-public:
-    void backtrack(vector<int>& candidates, int target, vector<vector<int>>& res, vector<int>& curr , int index){
-        int n=candidates.size();
-        if(index==n){
-            if(target==0){
-                res.push_back(curr);
-                
-            }
-            return;
-        }
-        if(candidates[index]<=target){
-            curr.push_back(candidates[index]);
-            backtrack(candidates,target-candidates[index],res,curr,index);
-            curr.pop_back();
-        }
-        backtrack(candidates, target,res, curr, index+1);
-    }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> curr;
-        backtrack(candidates, target, res, curr, 0);
-        return res;
-    }
-};
+// Last updated: 8/7/2026, 12:51:33 AM
+1class Solution {
+2public:
+3    void solve(vector<int>& candidates,vector<vector<int>>& res,vector<int> curr, int target, int idx){
+4        int n=candidates.size();
+5        if(idx==n){
+6            if(target==0){
+7                res.push_back(curr);
+8            }
+9            return;
+10        }
+11        if(candidates[idx]<=target){
+12            //picking 
+13            curr.push_back(candidates[idx]);
+14            solve(candidates, res, curr, target-candidates[idx],idx);
+15            curr.pop_back();
+16        }
+17        //not pick
+18        
+19        solve(candidates, res, curr, target, idx+1);
+20
+21    }
+22    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+23        vector<vector<int>> res;
+24        vector<int> curr;
+25        int sum=target;
+26        solve(candidates, res, curr, target,0);
+27        return res;
+28    }
+29};
