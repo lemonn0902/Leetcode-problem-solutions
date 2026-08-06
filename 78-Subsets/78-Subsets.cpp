045f0d74506d519b5cc498dd4f09951e4d2389e6
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-07 00:02:46 +0530

    File updated at 8/7/2026, 12:02:46 AM

--- 78-Subsets/78-Subsets.cpp
+++ 78-Subsets/78-Subsets.cpp
@@ -1,21 +1,26 @@
-// Last updated: 8/6/2026, 6:57:52 PM
+// Last updated: 8/7/2026, 12:02:46 AM
 1class Solution {
 2public:
-3    void backtrack(vector<int>& nums,vector<vector<int>>& res,int n,int i,vector<int>& sub){
-4        if(i==n){
-5            res.push_back(sub);
+3    void solve(vector<int>& nums,vector<vector<int>>& res,vector<int>& curr ,vector<bool>& used){
+4        if(curr.size()==nums.size()){
+5            res.push_back(curr);
 6            return;
 7        }
-8        sub.push_back(nums[i]);
-9        backtrack(nums, res,n,i+1, sub);
-10        sub.pop_back();
-11        backtrack(nums, res, n, i+1,sub);
-12    }
-13    vector<vector<int>> subsets(vector<int>& nums) {
-14        vector<vector<int>> ans;
-15        vector<int> curr;
-16        int n = nums.size();
-17        backtrack(nums, ans , n, 0, curr);
-18        return ans;
-19    }
-20};
+8        for(int i=0;i<nums.size();i++){
+9            if(used[i]) continue;
+10            used[i]=true;
+11            curr.push_back(nums[i]);
+12            solve(nums,res,curr, used);
+13            curr.pop_back();
+14            used[i]=false;
+15        }
+16    }
+17    vector<vector<int>> permute(vector<int>& nums) {
+18        vector<vector<int>> res;
+19        vector<int> curr;
+20        int n=nums.size();
+21        vector<bool> used(n,false);
+22        solve(nums, res, curr, used);
+23        return res;
+24    }
+25};
