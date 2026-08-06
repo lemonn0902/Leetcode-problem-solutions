ad90c8107897c4fce90224db71a3a975bdcaed39
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-06 23:27:03 +0530

    File updated at 8/6/2026, 11:27:03 PM

--- 90-SubsetsII/90-SubsetsII.cpp
+++ 90-SubsetsII/90-SubsetsII.cpp
@@ -1,21 +1,27 @@
-// Last updated: 10/26/2025, 4:28:05 PM
-class Solution {
-public:
-    void backtrack(vector<int>& nums, int n, int ind, vector<vector<int>>& res,vector<int> curr){
-        res.push_back(curr);
-        for(int i=ind;i<n;i++){
-            if(i!=ind&&nums[i]==nums[i-1]) continue;
-            curr.push_back(nums[i]);
-            backtrack(nums, n, i+1,res,curr);
-            curr.pop_back();
-        }
-    }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
-        vector<int> curr;
-        int n=nums.size();
-        backtrack(nums,n,0,res,curr);
-        return res;
-    }
-};
+// Last updated: 8/6/2026, 11:27:03 PM
+1class Solution {
+2public:
+3    void getSubsets(vector<int>& nums, int index, vector<vector<int>>& res,
+4                    vector<int>& current) {
+5        // base case:
+6        if (index == nums.size()) {
+7            if(find(res.begin(), res.end(),current)==res.end()){
+8                res.push_back(current);
+9            }
+10            
+11            return;
+12        }
+13
+14        current.push_back(nums[index]);
+15        getSubsets(nums, index + 1, res, current);
+16        current.pop_back();
+17        getSubsets(nums, index + 1,  res, current);
+18    }
+19    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+20        sort(nums.begin(), nums.end());
+21        vector<vector<int>> res;
+22        vector<int> current;
+23        getSubsets(nums, 0, res, current);
+24        return res;
+25    }
+26};
