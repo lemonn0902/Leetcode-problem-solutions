ba80c7e845ffd9ff94109526ce198a98ee7468f9
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-06 13:41:33 +0530

    File updated at 8/6/2026, 1:41:32 PM

--- 210-CourseScheduleII/210-CourseScheduleII.cpp
+++ 210-CourseScheduleII/210-CourseScheduleII.cpp
@@ -1,45 +1,34 @@
-// Last updated: 8/5/2026, 5:16:33 PM
+// Last updated: 8/6/2026, 1:41:32 PM
 1class Solution {
 2public:
-3    void transform(string& curr, unordered_set<string>& transformed) {
-4        int len = curr.size();
-5        for (int i = 0; i < len; i++) {
-6            char original = curr[i];
-7            for (char ch = 'a'; ch <= 'z'; ch++) {
-8                if (ch == original)
-9                    continue;
-10                curr[i] = ch;
-11                transformed.insert(curr);
-12            }
-13            curr[i] = original;
-14        }
-15    }
-16    int ladderLength(string beginWord, string endWord,
-17                     vector<string>& wordList) {
-18        queue<string> q;
-19        q.push(beginWord);
-20        int level = 1;
-21        unordered_set<string> st(wordList.begin(), wordList.end());
-22        if(st.find(endWord)==st.end()) return 0;
-23        while (!q.empty()) {
-24            int sz = q.size();
-25            while (sz--) {
-26                string curr = q.front();
-27                if (curr == endWord)
-28                    return level;
-29                q.pop();
-30                
-31                unordered_set<string> transformed;
-32                transform(curr, transformed);
-33                for (string s : transformed) {
-34                    if (st.find(s) != st.end()) {
-35                        q.push(s);
-36                        st.erase(s);
-37                    }
-38                }
-39            }
-40            level++;
-41        }
-42        return 0;
-43    }
-44};
+3    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+4        vector<vector<pair<int,int>>> adj(n+1);
+5        for(auto& e :times){
+6            int s=e[0];
+7            int t=e[1];
+8            int w=e[2];
+9            adj[s].push_back({t,w});
+10        }
+11        vector<int>dist(n+1,INT_MAX);
+12        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+13        dist[k]=0;
+14        pq.push({0,k});
+15        while(!pq.empty()){
+16            auto [d,node]=pq.top();
+17            pq.pop();
+18            if(d>dist[node]) continue;
+19            for(auto [next,w]:adj[node]){
+20                if(dist[node]+w<dist[next]){
+21                    dist[next]=dist[node]+w;
+22                    pq.push({dist[next],next});
+23                }
+24            }
+25        }
+26        int ans=0;
+27        for(int i=1;i<=n;i++){
+28            if(dist[i]==INT_MAX) return -1;
+29            ans=max(ans,dist[i]);
+30        }
+31        return ans;
+32    }
+33};
