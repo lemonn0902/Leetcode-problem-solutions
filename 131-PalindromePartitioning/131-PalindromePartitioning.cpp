869c510796938de8cbd9c71cf1668c512c2134f6
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-07 17:17:52 +0530

    File updated at 8/7/2026, 5:17:52 PM

--- 131-PalindromePartitioning/131-PalindromePartitioning.cpp
+++ 131-PalindromePartitioning/131-PalindromePartitioning.cpp
@@ -1,33 +1,27 @@
-// Last updated: 8/4/2025, 12:47:26 AM
-class Solution {
-public:
-    bool isPal(string s,int l,int r){
-        while (l < r) {
-            if (s[l++] != s[r--]) return false;
-        }
-        return true;
-    }
-    void backtrack(string s, vector<string> curr, vector<vector<string>>& res,int start){
-        int n=s.size();
-        if(start==s.size()){
-            res.push_back(curr);
-            return;
-        }
-        for(int i=start;i<n;i++){
-            if(isPal(s,start,i)){
-                curr.push_back(s.substr(start,i-start+1));
-                backtrack(s,curr,res,i+1);
-                curr.pop_back();
-            }
-        }
-        
-
-    }
-    vector<vector<string>> partition(string s) {
-        int n=s.size();
-        vector<string> curr;
-        vector<vector<string>> res;
-        backtrack(s,curr,res,0);
-        return res;
-    }
-};
+// Last updated: 8/7/2026, 5:17:52 PM
+1class Solution {
+2public:
+3    bool isPal(string s, int l, int r){
+4        while(l<r) if(s[l++]!=s[r--]) return false;
+5        return true;
+6    }
+7    void solve(string s,vector<vector<string>>& res,vector<string>& curr, int idx){
+8        int n = s.size();
+9        if(idx==n){
+10            res.push_back(curr);
+11            return ;
+12        }
+13        for(int end=idx;end<n;end++){
+14            if(!isPal(s,idx,end)) continue;
+15            curr.push_back(s.substr(idx, end-idx+1));
+16            solve(s,res,curr,end+1);
+17            curr.pop_back();
+18        }
+19    }
+20    vector<vector<string>> partition(string s) {
+21        vector<vector<string>> res;
+22        vector<string> curr;
+23         solve(s,res,curr,0);
+24         return res;
+25    }
+26};
