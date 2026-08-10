7bf96954503d6c61dd9fad372266415ec027d7a5
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-10 10:00:44 +0530

    File updated at 8/10/2026, 10:00:43 AM

--- 18-4Sum/18-4Sum.cpp
+++ 18-4Sum/18-4Sum.cpp
@@ -1,37 +1,20 @@
-// Last updated: 8/10/2026, 12:36:05 AM
+// Last updated: 8/10/2026, 10:00:43 AM
 1class Solution {
 2public:
-3
-4    int atMostK(vector<int>& nums, int k) {
-5        int left = 0;
-6        int right = 0;
-7        int count = 0;
-8
-9        unordered_map<int, int> mp;
-10
-11        while(right < nums.size()) {
-12
-13            mp[nums[right]]++;
-14
-15            while(mp.size() > k) {
-16                mp[nums[left]]--;
-17
-18                if(mp[nums[left]] == 0) {
-19                    mp.erase(nums[left]);
-20                }
-21
-22                left++;
-23            }
-24
-25            count += right - left + 1;
-26
-27            right++;
-28        }
-29
-30        return count;
-31    }
-32
-33    int subarraysWithKDistinct(vector<int>& nums, int k) {
-34        return atMostK(nums, k) - atMostK(nums, k - 1);
-35    }
-36};
+3    int f(vector<int>& nums, int i, int prev,vector<vector<int>>& dp ){
+4        int n=nums.size();
+5        if(i==n) return 0;
+6        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+7        int noTake=f(nums,i+1,prev,dp);
+8        int take=0;
+9        if(prev==-1||nums[i]>nums[prev]){
+10            take=1+f(nums,i+1,i,dp);
+11        }
+12        return dp[i][prev+1]=max(take, noTake);
+13    }
+14    int lengthOfLIS(vector<int>& nums) {
+15        int n=nums.size();
+16        vector<vector<int>> dp(n,vector<int>(n+1,-1));
+17        return f(nums,0,-1,dp);
+18    }
+19};
