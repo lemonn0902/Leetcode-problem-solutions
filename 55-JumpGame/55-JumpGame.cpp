9d9638195beeecf2b02964316d4cc53a07b00eca
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-10 22:48:37 +0530

    File updated at 8/10/2026, 10:48:37 PM

--- 55-JumpGame/55-JumpGame.cpp
+++ 55-JumpGame/55-JumpGame.cpp
@@ -1,15 +1,14 @@
-// Last updated: 7/12/2025, 4:21:10 PM
-class Solution {
-public:
-    bool canJump(vector<int>& nums) {
-        int maxIndexReached=0;
-        for(int i=0;i<nums.size();i++){
-            if(i > maxIndexReached) return false;
-            if(nums[i]+i>maxIndexReached)
-            maxIndexReached=nums[i]+i;
-            if(maxIndexReached>=nums.size()-1)
-            return true;
-        }
-        return false;
-    }
-};
+// Last updated: 8/10/2026, 10:48:37 PM
+1class Solution {
+2public:
+3    bool canJump(vector<int>& nums) {
+4        int n =nums.size();
+5        int maxi=0;
+6        for(int i=0;i<n;i++){
+7            if(i>maxi) return false;
+8            maxi = max(maxi, i+nums[i]);
+9            if(maxi>=n-1) return true;
+10        }
+11        return true;
+12    }
+13};
