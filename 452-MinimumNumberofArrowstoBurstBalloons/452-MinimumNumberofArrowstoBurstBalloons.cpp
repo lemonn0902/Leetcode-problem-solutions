216a4534f7d6b687335088126e665624e5491f86
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-10 20:46:47 +0530

    File updated at 8/10/2026, 8:46:47 PM

--- 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.cpp
+++ 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.cpp
@@ -1,21 +1,14 @@
-// Last updated: 8/10/2026, 8:17:58 PM
+// Last updated: 8/10/2026, 8:46:47 PM
 1class Solution {
 2public:
-3    int findMinArrowShots(vector<vector<int>>& points) {
-4        sort(points.begin(), points.end(), [](auto& a, auto& b){
-5            return a[1]<b[1];
-6        });
-7        int last=points[0][1];
-8        int count=1;
-9        for(auto& p: points){
-10            int start=p[0];
-11            int end= p[1];
-12            if(start>last){
-13                count++;
-14                last=end;
-15            }
-16            else continue;
-17        }
-18        return count;
-19    }
-20};
+3    int findContentChildren(vector<int>& g, vector<int>& s) {
+4        sort(g.begin(), g.end());
+5        sort(s.begin(), s.end());
+6        int child=0, cookie=0;
+7        while(child<g.size() && cookie<s.size()){
+8            if(s[cookie]>=g[child]) child++;
+9            cookie++;
+10        }
+11        return child;
+12    }
+13};
