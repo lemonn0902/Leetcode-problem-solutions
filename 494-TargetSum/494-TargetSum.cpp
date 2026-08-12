3e3518da57522e525799ffa0e16fc5b78cd864ab
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-12 14:06:30 +0530

    File updated at 8/12/2026, 2:06:29 PM

--- 494-TargetSum/494-TargetSum.cpp
+++ 494-TargetSum/494-TargetSum.cpp
@@ -1,24 +1,22 @@
-// Last updated: 8/7/2025, 12:56:57 AM
-class Solution {
-public:
-    int f(int index, int target, vector<int>& nums, int current_sum, vector<vector<int>>& dp, int offset){
-        if(index<0){
-            if(current_sum==target) return 1;
-            else return 0;
-        }
-        if(dp[index][offset+current_sum]!=-1) return dp[index][offset+current_sum];
-        int neg= f(index-1, target, nums, current_sum -nums[index], dp, offset);
-        int pos = f(index-1, target, nums, current_sum +nums[index], dp, offset);
-        return dp[index][offset+current_sum]=neg+pos;
-    }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int n= nums.size();
-        int tot=0;
-        for(int i=0;i<n;i++){
-            tot+=nums[i];
-        }
-        int offset= tot;
-        vector<vector<int>> dp(n, vector<int>(2*tot+1,-1));
-        return f(n-1, target, nums, 0 ,dp, offset);
-    }
-};
+// Last updated: 8/12/2026, 2:06:29 PM
+1class Solution {
+2public:
+3    int f(int idx,vector<int>& nums, int target,vector<vector<int>>& dp,int n,int sum, int offset){
+4        if(idx==n){
+5            if(sum==target) return 1;
+6            else return 0;
+7        }
+8        if(dp[idx][sum+offset]!=-1) return dp[idx][sum+offset];
+9        int plus=f(idx+1,nums,target,dp,n,sum+nums[idx],offset);
+10        int minus=f(idx+1,nums,target,dp,n,sum-nums[idx],offset);
+11        return dp[idx][sum+offset]=plus+minus;
+12    }
+13    int findTargetSumWays(vector<int>& nums, int target) {
+14        int n=nums.size();
+15        int tot=0;
+16        for(int n:nums) tot+=n;
+17        int offset=tot;
+18        vector<vector<int>> dp(n,vector<int>(2*tot+1,-1));
+19        return f(0,nums,target,dp,n,0,offset);
+20    }
+21};
