dbeb97f00640302dd92a6db1ad5983b1678de6d5
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-12 23:05:51 +0530

    File updated at 8/12/2026, 11:05:50 PM

--- 2958-LengthofLongestSubarrayWithatMostKFrequency/2958-LengthofLongestSubarrayWithatMostKFrequency.cpp
+++ 2958-LengthofLongestSubarrayWithatMostKFrequency/2958-LengthofLongestSubarrayWithatMostKFrequency.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/12/2026, 11:05:39 PM
+// Last updated: 8/12/2026, 11:05:50 PM
 1class Solution {
 2public:
 3    int maxSubarrayLength(vector<int>& nums, int k) {
@@ -7,7 +7,7 @@
 6        int l = 0;
 7        int r = 0;
 8        int n = nums.size();
-9        int maxi=INT_MIN;
+9        int maxi=0;
 10        while (r < n) {
 11            freq[nums[r]]++;
 12            while (freq[nums[r]] > k) {
