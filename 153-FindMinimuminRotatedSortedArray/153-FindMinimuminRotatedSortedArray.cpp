7891c7231ff95d9268899f70b585f67c2f0106dc
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-13 16:53:41 +0530

    File updated at 8/13/2026, 4:53:41 PM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cpp
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cpp
@@ -1,22 +1,21 @@
-// Last updated: 10/27/2025, 7:34:07 PM
-class Solution {
-public:
-    int findMin(vector<int>& nums) {
-        int low=0;
-        int high=nums.size()-1;
-        int mini=INT_MAX;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]>=nums[low]){
-                mini=min(mini, nums[low]);
-                low=mid+1;
-
-            }
-            else {
-                mini=min(mini, nums[mid]);
-                high=mid-1;
-            }
-        }
-        return mini;
-    }
-};
+// Last updated: 8/13/2026, 4:53:41 PM
+1class Solution {
+2public:
+3    int findMin(vector<int>& nums) {
+4        int n = nums.size();
+5        int lo=0, hi=n-1;
+6        int mini=INT_MAX;
+7        while(lo<=hi){
+8            int mid = lo+(hi-lo)/2;
+9            if(nums[mid]>=nums[lo]){
+10                mini= min(mini,nums[lo] );
+11                lo=mid+1;
+12            }
+13            else {
+14                mini=min(mini, nums [mid]);
+15                hi=mid-1;
+16            }
+17        }
+18        return mini;
+19    }
+20};
