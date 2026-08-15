35944e8cc9c8100891f873b67919b55a15b01277
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-16 01:28:49 +0530

    File updated at 8/16/2026, 1:28:49 AM

--- 347-TopKFrequentElements/347-TopKFrequentElements.cpp
+++ 347-TopKFrequentElements/347-TopKFrequentElements.cpp
@@ -1,20 +1,21 @@
-// Last updated: 5/17/2026, 2:50:42 PM
+// Last updated: 8/16/2026, 1:28:49 AM
 1class Solution {
 2public:
 3    vector<int> topKFrequent(vector<int>& nums, int k) {
-4        unordered_map<int,int> freq;
-5        for(int n : nums)
-6            freq[n]++;
-7
-8        priority_queue<pair<int,int>> pq;
-9        for(auto it : freq){
-10            pq.push({it.second, it.first}); 
+4        priority_queue<pair<int,int>> pq;
+5        unordered_map<int,int> mpp;
+6        for(int x:nums){
+7            mpp[x]++;
+8        }
+9        for(auto& it: mpp){
+10            pq.push({it.second, it.first});
 11        }
 12        vector<int> ans;
 13        while(k--){
-14            ans.push_back(pq.top().second); 
+14            int curr=pq.top().second;
 15            pq.pop();
-16        }
-17        return ans;
-18    }
-19};
+16            ans.push_back(curr);
+17        }
+18        return ans;
+19    }
+20};
