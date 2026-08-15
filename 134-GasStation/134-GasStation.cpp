c1f0730f8e750288a197f17dfae3ee2670028429
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-15 23:38:22 +0530

    File updated at 8/15/2026, 11:38:22 PM

--- 134-GasStation/134-GasStation.cpp
+++ 134-GasStation/134-GasStation.cpp
@@ -1,19 +1,21 @@
-// Last updated: 10/8/2025, 9:54:53 PM
-class Solution {
-public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int start=0;
-        int tank=0;
-        int totGas=0, totCost=0;
-        for(int i=0;i<gas.size();i++){
-            totGas+=gas[i];
-            totCost+=cost[i];
-            tank+= gas[i]-cost[i];
-            if(tank<0){
-                start=i+1;
-                tank=0;
-            }
-        }
-        return totGas<totCost? -1:start;
-    }
-};
+// Last updated: 8/15/2026, 11:38:22 PM
+1class Solution {
+2public:
+3    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+4        int gasSum=0;
+5        int costSum=0;
+6        for(int n:gas) gasSum+=n;
+7        for(int n:cost) costSum+=n;
+8        if(gasSum<costSum) return -1;
+9        int start=0;
+10        int tot=0;
+11        for(int i=0;i<gas.size();i++){
+12            tot+=(gas[i]-cost[i]);
+13            if(tot<0){
+14                tot=0;
+15                start=i+1;
+16            }
+17        }
+18        return start;
+19    }
+20};
