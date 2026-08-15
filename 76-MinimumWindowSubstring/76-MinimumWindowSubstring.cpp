ca83db794080d59516c78dfca0394c5bd2df85a2
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-15 20:49:21 +0530

    File updated at 8/15/2026, 8:49:21 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.cpp
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.cpp
@@ -1,38 +1,39 @@
-// Last updated: 7/28/2025, 2:20:11 PM
-class Solution {
-public:
-    string minWindow(string s, string t) {
-        unordered_map<char,int> tFreq;
-        unordered_map<char,int> freq;
-        int m=s.size();
-        int n=t.size();
-        if(n>m) return "";
-        int l=0;
-        int r=0;
-        int minLen=INT_MAX;int startIndex=0; int formed=0; 
-        for(int i=0;i<n;i++){
-            tFreq[t[i]]++;
-        }
-        int req=tFreq.size();
-        while(r<m){
-            freq[s[r]]++;
-            if(tFreq.count(s[r])&&tFreq[s[r]]==freq[s[r]]){
-                formed++;
-            }
-            //now we try to shrink from left 
-            while(formed==req){
-                if(1+r-l<minLen){
-                    minLen=1+r-l;
-                    startIndex=l;
-                }
-                freq[s[l]]--;
-                if (tFreq.count(s[l]) && freq[s[l]] < tFreq[s[l]]) {
-                    formed--;
-                }
-                l++;
-            }
-            r++;
-        }
-        return minLen==INT_MAX? "": s.substr(startIndex, minLen);
-    }
-};
+// Last updated: 8/15/2026, 8:49:21 PM
+1class Solution {
+2public:
+3    string minWindow(string s, string t) {
+4        int m = s.size();
+5        int n = t.size();
+6        if (n > m)
+7            return "";
+8        int mini = INT_MAX;
+9        unordered_map<char, int> tfreq;
+10        unordered_map<char, int> freq;
+11        for (int i = 0; i < n; i++) {
+12            tfreq[t[i]]++;
+13        }
+14        int req = tfreq.size();
+15        int l = 0;
+16        int r = 0;
+17        int startIndex = 0;
+18        int formed = 0;
+19        while (r < m) {
+20            freq[s[r]]++;
+21            if (tfreq.count(s[r]) && tfreq[s[r]] == freq[s[r]]) {
+22                formed++;
+23            }
+24            while (formed == req) {
+25                if (1 + r - l < mini) {
+26                    mini = 1 + r - l;
+27                    startIndex = l;
+28                }
+29                freq[s[l]]--;
+30                if (tfreq[s[l]] && tfreq[s[l]] > freq[s[l]])
+31                    formed--;
+32                l++;
+33            }
+34            r++;
+35        }
+36        return mini == INT_MAX ? "" : s.substr(startIndex, mini);
+37    }
+38};
