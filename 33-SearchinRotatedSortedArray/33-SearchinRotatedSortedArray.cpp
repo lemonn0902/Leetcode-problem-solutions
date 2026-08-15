a3f531833487c31807f4b55a6dbd70d612cf1e2e
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-15 22:50:26 +0530

    File updated at 8/15/2026, 10:50:26 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
@@ -1,24 +1,35 @@
-// Last updated: 8/13/2026, 4:34:39 PM
+// Last updated: 8/15/2026, 10:50:26 PM
 1class Solution {
 2public:
 3    int search(vector<int>& nums, int target) {
 4        int n = nums.size();
-5        int lo=0, hi=n-1;
-6        while(lo<=hi){
-7            int mid = lo+(hi-lo)/2;
-8            if(nums[mid]==target) return mid;
-9            if(nums[mid]>=nums[lo]){
-10                //sorted half = first
-11                if(nums[lo]<=target && target< nums[mid]) hi=mid-1;
-12                else lo=mid+1; 
-13
-14            }
-15            else{
-16                //sorted half = second
-17                if(nums[mid]< target && target <= nums[hi]) lo=mid+1;
-18                else hi=mid-1;
-19            }
-20        }
-21        return -1;
-22    }
-23};
+5        int lo = 0, hi = n - 1;
+6
+7        while (lo <= hi) {
+8            int m = lo + (hi - lo) / 2;
+9
+10            if (nums[m] == target)
+11                return m;
+12
+13            // Left half is sorted
+14            if (nums[lo] <= nums[m]) {
+15
+16                if (nums[lo] <= target && target < nums[m])
+17                    hi = m - 1;
+18                else
+19                    lo = m + 1;
+20            }
+21
+22            // Right half is sorted
+23            else {
+24
+25                if (nums[m] < target && target <= nums[hi])
+26                    lo = m + 1;
+27                else
+28                    hi = m - 1;
+29            }
+30        }
+31
+32        return -1;
+33    }
+34};
