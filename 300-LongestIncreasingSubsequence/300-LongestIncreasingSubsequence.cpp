89fe7e227146366d5b819c7cd02194c7835f5087
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-16 16:24:51 +0530

    File updated at 8/16/2026, 4:24:51 PM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.cpp
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/16/2026, 4:23:54 PM
+// Last updated: 8/16/2026, 4:24:51 PM
 1/**
 2 * Definition for singly-linked list.
 3 * struct ListNode {
@@ -12,21 +12,24 @@
 11class Solution {
 12public:
 13    ListNode* mergeKLists(vector<ListNode*>& lists) {
-14        priority_queue<pair<int,ListNode*> , vector<pair<int,ListNode*>>, greater<>> pq;
-15        for(auto it:lists){
-16            if(it) pq.push({it->val, it});
-17        }
-18        ListNode dummy(0);
-19        ListNode* temp=&dummy;
-20        while(!pq.empty()){
-21            auto [val, node]= pq.top();
-22            pq.pop();
-23            temp->next=node;
-24            temp=temp->next;
-25            if(node->next){
-26                pq.push({node->next->val,node->next});
-27            }
-28        }
-29        return dummy.next;
-30    }
-31};
+14        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>,
+15                       greater<>>
+16            pq;
+17        for (auto it : lists) {
+18            if (it)
+19                pq.push({it->val, it});
+20        }
+21        ListNode* dummy = new ListNode(0);
+22        ListNode* temp = dummy;
+23        while (!pq.empty()) {
+24            auto [val, node] = pq.top();
+25            pq.pop();
+26            temp->next = node;
+27            temp = temp->next;
+28            if (node->next) {
+29                pq.push({node->next->val, node->next});
+30            }
+31        }
+32        return dummy->next;
+33    }
+34};
