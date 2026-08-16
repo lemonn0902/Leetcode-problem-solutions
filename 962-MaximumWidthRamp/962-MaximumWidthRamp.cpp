9d48b739fab6184f4ac761af2d4229e7904e76e4
lemon <155637262+lemonn0902@users.noreply.github.com>
2026-08-16 16:33:29 +0530

    File updated at 8/16/2026, 4:33:29 PM

--- 962-MaximumWidthRamp/962-MaximumWidthRamp.cpp
+++ 962-MaximumWidthRamp/962-MaximumWidthRamp.cpp
@@ -1,28 +1,27 @@
-// Last updated: 8/16/2026, 2:39:17 PM
+// Last updated: 8/16/2026, 4:33:29 PM
 1class Solution {
 2public:
 3    int largestRectangleArea(vector<int>& heights) {
-4        stack<pair<int,int>> st; //index, no
-5        int n=heights.size();
-6        int maxi=INT_MIN;
-7        for(int i=0;i<n;i++){
-8            int start=i;
-9            while(!st.empty() && heights[i]<st.top().second){
-10                int idx= st.top().first;
-11                int h=st.top().second;
-12                st.pop();
-13                maxi=max(maxi, h*(i-idx));
-14                start=idx;
-15            }
-16            st.push({start, heights[i]});
-17        }
-18        while (!st.empty()) {
-19            int index = st.top().first;
-20            int height = st.top().second;
+4        stack<pair<int,int>> st; //idx, no
+5        int maxi=INT_MIN;
+6        for(int i=0;i<heights.size();i++){
+7            int start=i;
+8            while(!st.empty() && heights[i]<st.top().second){
+9                int idx=st.top().first;
+10                int h=st.top().second;
+11                st.pop();
+12                maxi=max(maxi, h*(i-idx));
+13                start=idx;
+14            }
+15            st.push({start, heights[i]});
+16        }
+17        int n =heights.size();
+18        while(!st.empty()){
+19            int h=st.top().second;
+20            int i=st.top().first;
 21            st.pop();
-22
-23            maxi = max(maxi, height * (n - index));
-24        }
-25        return maxi;
-26    }
-27};
+22            maxi=max(maxi, h*(n-i));
+23        }
+24        return maxi;
+25    }
+26};
